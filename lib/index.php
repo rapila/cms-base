e2dc04968b8a95a462b19bcee2c001aa18bdf82d
Raphael Schweikert <any@sabberworm.com>
2009-01-13 09:21:05 +0000

    moved main.php content back to index.php

--- lib/index.php
+++ lib/index.php
@@ -1,2 +1,10 @@
 <?php
-require_once 'includes/main.php';
+try {
+  require_once 'inc.php';
+  set_error_handler(array('ErrorHandler', "handleError"));
+  $oManager = Manager::getManager();
+  $oManager->render();
+  Autoloader::saveIncludeCache();
+} catch (Exception $oException) {
+  ErrorHandler::handleException($oException);
+}
