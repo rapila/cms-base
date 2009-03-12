ca8eee4fff27cfc7445fe0b16d321368a4cd4dc1
Jürg Messmer <jmessmer@bluewin.ch>
2009-03-12 11:35:52 +0000

    short month name added to getMonthNameById

--- lib/classes/CalendarUtil.php
+++ lib/classes/CalendarUtil.php
@@ -5,23 +5,23 @@
 class CalendarUtil {
   
   private static $MONTH_NAMES = array('de' => 
-                                  array(1 => 'Januar',
-                                        2 => 'Februar',
-                                        3 => 'März',
-                                        4 => 'April',
-                                        5 => 'Mai',
-                                        6 => 'Juni',
-                                        7 => 'Juli',
-                                        8 => 'August',
-                                        9 => 'September',
-                                        10 => 'Oktober',
-                                        11 => 'November',
-                                        12 => 'Dezember'
+                                  array(1 => array('long'=> 'Januar',   'short' => 'Jan'),
+                                        2 => array('long'=> 'Februar',  'short' => 'Feb'),
+                                        3 => array('long'=> 'März',     'short' => 'Mär'),
+                                        4 => array('long'=> 'April',    'short' => 'Apr'),
+                                        5 => array('long'=> 'Mai',      'short' => 'Mai'),
+                                        6 => array('long'=> 'Juni',     'short' => 'Jun'),
+                                        7 => array('long'=> 'Juli',     'short' => 'Jul'),
+                                        8 => array('long'=> 'August',   'short' => 'Aug.'),
+                                        9 => array('long'=> 'September','short' => 'Sep'),
+                                        10 => array('long'=> 'Oktober', 'short' => 'Okt'),
+                                        11 => array('long'=> 'November','short' => 'Nov'),
+                                        12 => array('long'=> 'Dezember','short' => 'Dez')
                                         )
                                       );
                                       
-  public static function getMonthNameByMonthId($iMonthId, $sLanguageId=null) {
+  public static function getMonthNameByMonthId($iMonthId, $sLanguageId=null, $sType='long') {
     $sLanguageId = $sLanguageId !== null ? $sLanguageId : Session::language();
-    return self::$MONTH_NAMES[$sLanguageId][$iMonthId];
+    return self::$MONTH_NAMES[$sLanguageId][$iMonthId][$sType];
   }
 }
