7726bf3dc6072bc7aabebfc3b8ea431c086e1d4a
Jürg Messmer <jmessmer@bluewin.ch>
2009-07-06 08:01:06 +0000

    cleanup and flash.valid_email

--- lib/model/UserGroupPeer.php
+++ lib/model/UserGroupPeer.php
@@ -8,16 +8,9 @@
 
 
 /**
- * Skeleton subclass for performing query and update operations on the 'users_groups' table.
- *
- * 
- *
- * You should add additional methods to this class to meet the
- * application requirements.  This class will only be generated as
- * long as it does not already exist in the output directory.
- *
  * @package    model
  */
 class UserGroupPeer extends BaseUserGroupPeer {
 
-} // UserGroupPeer
+}
+
