b609c680d60c47f3c70c4dc5a73f79c5511a229a
Raphael Schweikert <any@sabberworm.com>
2009-07-15 13:55:21 +0000

    added html5 “doctypes”

--- lib/classes/XHTMLOutput.php
+++ lib/classes/XHTMLOutput.php
@@ -5,8 +5,10 @@ define("XHTML_TEMPLATE_CHARSET_IDENTIFIER", TemplateIdentifier::constructIdentif
 
 define("DOCTYPE_HTML_4_STRICT", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01//EN\" \"http://www.w3.org/TR/html4/strict.dtd\">\n<html lang=\"".XHTML_TEMPLATE_LANGUAGE_IDENTIFIER."\">\n");
 define("DOCTYPE_HTML_4_TRANSITIONAL", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\" \"http://www.w3.org/TR/html4/loose.dtd\">\n<html lang=\"".XHTML_TEMPLATE_LANGUAGE_IDENTIFIER."\">\n");
+define("DOCTYPE_HTML_5", "<!DOCTYPE HTML>\n<html lang=\"".XHTML_TEMPLATE_LANGUAGE_IDENTIFIER."\">\n");
 define("DOCTYPE_XHTML_STRICT", "<?xml version=\"1.0\" encoding=\"".XHTML_TEMPLATE_CHARSET_IDENTIFIER."\" ?>\n<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\">\n<html xmlns=\"http://www.w3.org/1999/xhtml\" xml:lang=\"".XHTML_TEMPLATE_LANGUAGE_IDENTIFIER."\">\n");
 define("DOCTYPE_XHTML_TRANSITIONAL", "<?xml version=\"1.0\" encoding=\"".XHTML_TEMPLATE_CHARSET_IDENTIFIER."\" ?>\n<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\">\n<html xmlns=\"http://www.w3.org/1999/xhtml\" xml:lang=\"".XHTML_TEMPLATE_LANGUAGE_IDENTIFIER."\">\n");
+define("DOCTYPE_XHTML_5", "<?xml version=\"1.0\" encoding=\"".XHTML_TEMPLATE_CHARSET_IDENTIFIER."\" ?>\n<!DOCTYPE HTML>\n<html xmlns=\"http://www.w3.org/1999/xhtml\" xml:lang=\"".XHTML_TEMPLATE_LANGUAGE_IDENTIFIER."\">\n");
 
 
 class XHTMLOutput {
@@ -14,6 +16,8 @@ class XHTMLOutput {
   const SETTING_TRANSITIONAL = 'transitional';
   const SETTING_HTML_4_STRICT = 'html4_strict';
   const SETTING_HTML_4_TRANSITIONAL = 'html4_transitional';
+  const SETTING_HTML_5 = 'html5';
+  const SETTING_XHTML_5 = 'xhtml5';
   const SETTING_NONE = 'none';
   
   private $sCharset;
@@ -31,10 +35,10 @@ class XHTMLOutput {
     if($this->sSetting === null) {
       $this->sSetting = Settings::getSetting('frontend', 'doctype', 'none');
     }
-    if($this->sSetting === self::SETTING_NONE) {
+    if($this->sSetting === self::SETTING_NONE || $this->sSetting === self::SETTING_HTML_4_STRICT || $this->sSetting === self::SETTING_HTML_4_TRANSITIONAL || $this->sSetting === self::SETTING_HTML_5) {
       return;
     }
-    if(($this->sSetting === self::SETTING_TRANSITIONAL || $this->sSetting === self::SETTING_STRICT) && @stristr(@$_SERVER["HTTP_ACCEPT"], "application/xhtml+xml")) {
+    if(@stristr(@$_SERVER["HTTP_ACCEPT"], "application/xhtml+xml")) {
       if(preg_match("/application\/xhtml\+xml;q=([01]|0\.\d{1,3}|1\.0)/i", $_SERVER["HTTP_ACCEPT"], $matches)) {
         $xhtml_q = $matches[1];
         if(preg_match("/text\/html;q=q=([01]|0\.\d{1,3}|1\.0)/i", $_SERVER["HTTP_ACCEPT"], $matches)) {
@@ -62,9 +66,13 @@ class XHTMLOutput {
         case (self::SETTING_TRANSITIONAL):
           $sDoctype = DOCTYPE_XHTML_TRANSITIONAL;
           break;
+        case (self::SETTING_XHTML_5):
+  	      ob_start(array($this, "fixCodeForXhtml5"));
+          $sDoctype = DOCTYPE_XHTML_5;
+          break;
       }
     } else {
-  	  ob_start(array($this, "fixCodeForHtml4"));
+  	  ob_start(array($this, "fixCodeForHtml"));
       switch($this->sSetting) {
         case (self::SETTING_STRICT):
         case (self::SETTING_HTML_4_STRICT):
@@ -73,6 +81,10 @@ class XHTMLOutput {
         case (self::SETTING_TRANSITIONAL):
         case (self::SETTING_HTML_4_TRANSITIONAL):
           $sDoctype = DOCTYPE_HTML_4_TRANSITIONAL;
+          break;
+        case (self::SETTING_XHTML_5):
+        case (self::SETTING_HTML_5):
+          $sDoctype = DOCTYPE_HTML_5;
           break;
       }
     }
@@ -90,7 +102,11 @@ class XHTMLOutput {
     $oTemplate->render();
   }
   
-  public function fixCodeForHtml4($sBuffer) {
-    return (preg_replace("!\s*/>!", ">", $sBuffer));
+  public function fixCodeForHtml($sBuffer) {
+    return preg_replace("!\s*/>!", ">", $sBuffer);
+  }
+  
+  public function fixCodeForXhtml5($sBuffer) {
+    return str_replace("&nbsp;", '&#160;', $sBuffer);
   }
 }
