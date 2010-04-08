55d1ea73a4aaebf80633c32a102c910b172b3e46
Jürg Messmer <jmessmer@bluewin.ch>
2010-04-08 13:00:52 +0000

    getFileInfo method added

--- lib/model/Document.php
+++ lib/model/Document.php
@@ -5,63 +5,63 @@ require_once 'model/om/BaseDocument.php';
 
 /**
  * @package model
- */	
+ */ 
 class Document extends BaseDocument {
-  
-  private static $DOCUMENT_CATEGORIES = array();
-  
-  private $iDataSize = null;
-  
-  public function getMimetype() {
-    return $this->getDocumentType()->getMimetype();
-  }
-  
-  public function getExtension() {
-    return $this->getDocumentType()->getExtension();
-  }
-  
-  public function getDocumentKind() {
-    return $this->getDocumentType()->getDocumentKind();
-  }
-  
-  public function getNameAndExtension() {
-    return $this->getName().' ['.$this->getExtension().']';
-  }
-  
-  public function getFullName() {
-    return $this->getName().'.'.$this->getExtension();
-  }
-  
-  public function delete($con = null) {
-    if(ReferencePeer::hasReference($this)) {
-      throw new Exception("Exception in ".__METHOD__.": tried removing an instance from the database even though it is still referenced.");
-    }
-    TagPeer::deleteTagsForObject($this);
-    ReferencePeer::removeReferences($this);
-    return parent::delete($con);
-  }
-  
-  public function isImage() {
-    return $this->getDocumentType()->isImageType();
-  }
-  
-  public function getDisplayUrl($aUrlParameters = array(), $sFileModule = 'display_document') {
-    return LinkUtil::link(array($sFileModule, $this->getId()), "FileManager", $aUrlParameters);
-  }
-  
-  public function getCategoryName() {
-    if($this->getDocumentCategory()) {
-      return $this->getDocumentCategory()->getName();
-    }
-    return null;
-  }
-  
-  public function getDataSize($oConnection = null) {
-    if($this->iDataSize !== null) {
-      return $this->iDataSize;
-    }
+	
+	private static $DOCUMENT_CATEGORIES = array();
+	
+	private $iDataSize = null;
+	
+	public function getMimetype() {
+		return $this->getDocumentType()->getMimetype();
+	}
+	
+	public function getExtension() {
+		return $this->getDocumentType()->getExtension();
+	}
+	
+	public function getDocumentKind() {
+		return $this->getDocumentType()->getDocumentKind();
+	}
+	
+	public function getNameAndExtension() {
+		return $this->getName().' ['.$this->getExtension().']';
+	}
+	
+	public function getFullName() {
+		return $this->getName().'.'.$this->getExtension();
+	}
+	
+	public function delete($con = null) {
+		if(ReferencePeer::hasReference($this)) {
+			throw new Exception("Exception in ".__METHOD__.": tried removing an instance from the database even though it is still referenced.");
+		}
+		TagPeer::deleteTagsForObject($this);
+		ReferencePeer::removeReferences($this);
+		return parent::delete($con);
+	}
+	
+	public function isImage() {
+		return $this->getDocumentType()->isImageType();
+	}
+	
+	public function getDisplayUrl($aUrlParameters = array(), $sFileModule = 'display_document') {
+		return LinkUtil::link(array($sFileModule, $this->getId()), "FileManager", $aUrlParameters);
+	}
+	
+	public function getCategoryName() {
+		if($this->getDocumentCategory()) {
+			return $this->getDocumentCategory()->getName();
+		}
+		return null;
+	}
+	
+	public function getDataSize($oConnection = null) {
+		if($this->iDataSize !== null) {
+			return $this->iDataSize;
+		}
 		$oCriteria = $this->buildPkeyCriteria();
-    $oCriteria->addSelectColumn('OCTET_LENGTH(data)');
+		$oCriteria->addSelectColumn('OCTET_LENGTH(data)');
 		try {
 			$rs = DocumentPeer::doSelectRS($oCriteria, $oConnection);
 			$rs->next();
@@ -69,40 +69,44 @@ class Document extends BaseDocument {
 		} catch (Exception $e) {
 			throw new PropelException("Error loading value for [size] column on demand.", $e);
 		}
-  }
-  
-  /**
-  * Shortcut for getDisplayUrl(array(), 'display_document');
-  * @deprecated use Document->getDisplayUrl() instead
-  * @todo remove
-  */
-  public function getLink() {
-    return $this->getDisplayUrl(array(), 'display_document');
-  }
-  
-  public function save($oConnection = null) {
-    if(Session::getSession()->isAuthenticated()) {
-      $this->setUpdatedBy(Session::getSession()->getUserId());
-    }
-    $this->setUpdatedAt(date('c'));
+	}
 
-    if($this->isNew()) {
-      if(Session::getSession()->isAuthenticated()) {
-        $this->setCreatedBy(Session::getSession()->getUserId());
-      }
-      $this->setCreatedAt(date('c'));
-    }
-    return parent::save($oConnection);
-  }
-  
-  public function getDocumentCategory($con=null) {
-    if(!isset(self::$DOCUMENT_CATEGORIES[$this->getDocumentCategoryId()])) {
-      self::$DOCUMENT_CATEGORIES[$this->getDocumentCategoryId()] = parent::getDocumentCategory($con);
-    }
-    return self::$DOCUMENT_CATEGORIES[$this->getDocumentCategoryId()];
-  }
-  
-  public function setDocumentCategoryId($mCategoryId) {
-    parent::setDocumentCategoryId(is_numeric($mCategoryId) && $mCategoryId > 0 ? $mCategoryId : null);
-  }
+	public function getFileInfo() {
+		return $this->getExtension().' / '.DocumentUtil::getDocumentSize($this->getDataSize());
+	}
+	
+	/**
+	* Shortcut for getDisplayUrl(array(), 'display_document');
+	* @deprecated use Document->getDisplayUrl() instead
+	* @todo remove
+	*/
+	public function getLink() {
+		return $this->getDisplayUrl(array(), 'display_document');
+	}
+	
+	public function save($oConnection = null) {
+		if(Session::getSession()->isAuthenticated()) {
+			$this->setUpdatedBy(Session::getSession()->getUserId());
+		}
+		$this->setUpdatedAt(date('c'));
+
+		if($this->isNew()) {
+			if(Session::getSession()->isAuthenticated()) {
+				$this->setCreatedBy(Session::getSession()->getUserId());
+			}
+			$this->setCreatedAt(date('c'));
+		}
+		return parent::save($oConnection);
+	}
+	
+	public function getDocumentCategory($con=null) {
+		if(!isset(self::$DOCUMENT_CATEGORIES[$this->getDocumentCategoryId()])) {
+			self::$DOCUMENT_CATEGORIES[$this->getDocumentCategoryId()] = parent::getDocumentCategory($con);
+		}
+		return self::$DOCUMENT_CATEGORIES[$this->getDocumentCategoryId()];
+	}
+	
+	public function setDocumentCategoryId($mCategoryId) {
+		parent::setDocumentCategoryId(is_numeric($mCategoryId) && $mCategoryId > 0 ? $mCategoryId : null);
+	}
 } // Document
