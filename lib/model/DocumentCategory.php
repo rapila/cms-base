0fc23d371eea3bb76d335ebbde4c9245df717e9d
Jürg Messmer <jmessmer@bluewin.ch>
2010-04-13 04:41:53 +0000

    getDocumentLinkData for DISPLAY_TYPE_URL getUpdatedAtFormatted for updated_at in list

--- lib/model/DocumentCategory.php
+++ lib/model/DocumentCategory.php
@@ -17,5 +17,12 @@ class DocumentCategory extends BaseDocumentCategory {
 			return $this->getMaxWidth().'px';
 		}
 	}
+	
+	public function getDocumentLinkData() {
+		$aArray = array();
+		$aArray[] = $this->getDocumentCount().' '.StringPeer::getString('documents');
+		$aArray[] = LinkUtil::link(array('documents'), 'AdminManager', array('document_category_id' => $this->getId()));
+		return $aArray;
+	}
 }
 
--- lib/model/Link.php
+++ lib/model/Link.php
@@ -4,16 +4,20 @@ require_once 'model/om/BaseLink.php';
 
 /**
  * @package model
- */	
+ */ 
 class Link extends BaseLink {
-  
-  public function delete($con = null) {
-    if(ReferencePeer::hasReference($this)) {
-      throw new Exception("Exception in ".__METHOD__.": tried removing an instance from the database even though it is still referenced.");
-    }
-    TagPeer::deleteTagsForObject($this);
-    ReferencePeer::removeReferences($this);
-    return parent::delete($con);
-  }
+	
+	public function delete($con = null) {
+		if(ReferencePeer::hasReference($this)) {
+			throw new Exception("Exception in ".__METHOD__.": tried removing an instance from the database even though it is still referenced.");
+		}
+		TagPeer::deleteTagsForObject($this);
+		ReferencePeer::removeReferences($this);
+		return parent::delete($con);
+	}
+
+	public function getUpdatedAtFormatted() {
+		return LocaleUtil::localizeDate($this->getUpdatedAt('c'));
+	}
 }
 
