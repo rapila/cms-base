555ac8306fcd4f4f20852950bcd788c57ef829ac
Jürg Messmer <jmessmer@bluewin.ch>
2010-05-23 19:25:19 +0000

    new tree widget module added and old page_list removed

---
+++ modules/widget/tree/TreeWidgetModule.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * @package modules.widget
+ */
+class TreeWidgetModule extends PersistentWidgetModule {
+	private $oDelegate = null;
+	private $bIsOrdered = false;
+	
+	public function setDelegate($oDelegate) {
+		$this->oDelegate = $oDelegate;
+	}
+	
+	public function getDelegate() {
+		return $this->oDelegate;
+	}
+	
+	public function setOrdered($bIsOrdered)
+	{
+	    $this->bIsOrdered = $bIsOrdered;
+	}
+
+	public function isOrdered()
+	{
+	    return $this->bIsOrdered;
+	}
+	
+	public function doWidget() {
+		$oListTag = new TagWriter($this->bIsOrdered ? 'ol' : 'ul');
+		$oListTag->addToParameter('class', 'ui-tree');
+		$oListTag->setParameter('data-widget-session', $this->sPersistentSessionKey);
+		$oListTag->setParameter('data-widget-type', $this->getModuleName());
+		return $oListTag->parse();
+	}
+	
+	public function listChildren($aParentData) {
+		return $this->oDelegate->listChildren($aParentData);
+	}
+}
