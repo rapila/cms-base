28382ebaec04da9fca604e937b14250378c453ac
Raphael Schweikert <any@sabberworm.com>
2010-07-14 22:14:23 +0000

    working file uploads, yeah

--- modules/widget/file_upload/FileUploadWidgetModule.php
+++ modules/widget/file_upload/FileUploadWidgetModule.php
@@ -5,9 +5,29 @@ class FileUploadWidgetModule extends WidgetModule {
 	}
 	
 	public function uploadFile($sFileData, $aOptions) {
+		$sFileData = base64_decode($sFileData);
 		$oDocument = new Document();
+		$sFileName = $aOptions['name'];
+		$aName = explode('.', $sFileName);
+		$sExtension = null;
+		if(count($aName) > 1) {
+			$sExtension = array_pop($aName);
+			$sFileName = implode('.', $aName);
+		}
+		$oDocumentType = DocumentTypePeer::getDocumentTypeByMimetype($aOptions['type']);
+		if($oDocumentType === null && $sExtension !== null) {
+			$oDocumentType = DocumentTypePeer::getDocumentTypeByExtension($sExtension);
+		}
+		if($oDocumentType === null) {
+			throw new LocalizedException("widget.file_upload.document_type_not_found");
+		}
 		$oDocument->setData($sFileData);
-		$oDocument->setName($aOptions['name']);
-		return $oDocument->save();
+		$oDocument->setName($sFileName);
+		$oDocument->setDocumentType($oDocumentType);
+		if($aOptions['document_category_id']) {
+			$oDocument->setDocumentCategoryId($aOptions['document_category_id']);
+		}
+		$oDocument->save();
+		return $oDocument->getId();
 	}
 }
