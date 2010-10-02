b3582b304cd6a3ebed7cc7edb081d9dfc6efc451
Jürg Messmer <jmessmer@bluewin.ch>
2010-10-02 11:41:36 +0000

    config param admin:allow_select_all_users: false added in order to prevent all users select, default backend_login_enabled users are shown

--- modules/widget/user_kind_input/UserKindInputWidgetModule.php
+++ modules/widget/user_kind_input/UserKindInputWidgetModule.php
@@ -5,12 +5,18 @@
 class UserKindInputWidgetModule extends PersistentWidgetModule {
 
 	private $sSelectedUserKind;
+	const IS_BACKEND_LOGIN_ENABLED = '1';
+	const IS_FRONTEND_USER = '2';
 		
 	public function getUserKinds() {
-		return array(CriteriaListWidgetDelegate::SELECT_ALL => StringPeer::getString('widget.user_kind.all'),
-		            '1' => StringPeer::getString('widget.user.backend'), 
-		            '2' => StringPeer::getString('widget.user.frontend')
+		$aOptions = array(
+		            self::IS_BACKEND_LOGIN_ENABLED => StringPeer::getString('widget.user.backend'), 
+		            self::IS_FRONTEND_USER => StringPeer::getString('widget.user.frontend')
 		            );
+		if($this->allowSelectAll()) {
+			$aOptions = array_merge(array(CriteriaListWidgetDelegate::SELECT_ALL => StringPeer::getString('widget.user_kind.all')), $aOptions);
+		}
+		return $aOptions;
 	}
 	
 	public function setSelectedUserKind($sSelectedUserKind) {
@@ -23,8 +29,15 @@ class UserKindInputWidgetModule extends PersistentWidgetModule {
 	public function getElementType() {
 		return 'select';
 	}
+	
+	private function allowSelectAll() {
+		return Settings::getSetting('admin', 'allow_select_all_users', true);
+	}
 
 	public function getSelectedUserKind() {
+		if( $this->sSelectedUserKind === null && $this->allowSelectAll() === false) {
+			$this->sSelectedUserKind = self::IS_BACKEND_LOGIN_ENABLED;
+		}
 		return $this->sSelectedUserKind;
 	}
 }
