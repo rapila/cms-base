db74f7353eb49f47cbaee203366f288b26735575
Jürg Messmer <jmessmer@bluewin.ch>
2010-10-16 08:01:02 +0000

    allowSelectAll handling removed

--- modules/widget/user_kind_input/UserKindInputWidgetModule.php
+++ modules/widget/user_kind_input/UserKindInputWidgetModule.php
@@ -9,14 +9,10 @@ class UserKindInputWidgetModule extends PersistentWidgetModule {
 	const IS_FRONTEND_USER = '2';
 		
 	public function getUserKinds() {
-		$aOptions = array(
-		            self::IS_BACKEND_LOGIN_ENABLED => StringPeer::getString('widget.user.backend'), 
-		            self::IS_FRONTEND_USER => StringPeer::getString('widget.user.frontend')
-		            );
-		if($this->allowSelectAll()) {
-			$aOptions = array_merge(array(CriteriaListWidgetDelegate::SELECT_ALL => StringPeer::getString('widget.user_kind.all')), $aOptions);
-		}
-		return $aOptions;
+		return array(CriteriaListWidgetDelegate::SELECT_ALL => StringPeer::getString('widget.user_kind.all'),
+								self::IS_BACKEND_LOGIN_ENABLED => StringPeer::getString('widget.user.backend'), 
+								self::IS_FRONTEND_USER => StringPeer::getString('widget.user.frontend')
+								);
 	}
 	
 	public function setSelectedUserKind($sSelectedUserKind) {
@@ -29,15 +25,8 @@ class UserKindInputWidgetModule extends PersistentWidgetModule {
 	public function getElementType() {
 		return 'select';
 	}
-	
-	public function allowSelectAll() {
-		return Settings::getSetting('admin', 'allow_select_all_users', true);
-	}
 
 	public function getSelectedUserKind() {
-		if( $this->sSelectedUserKind === null && $this->allowSelectAll() === false) {
-			$this->sSelectedUserKind = self::IS_BACKEND_LOGIN_ENABLED;
-		}
 		return $this->sSelectedUserKind;
 	}
 }
