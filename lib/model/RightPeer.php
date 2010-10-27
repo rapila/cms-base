ddf22340e09055c37d297687d97ce79153c47062
Jürg Messmer <jmessmer@bluewin.ch>
2010-10-27 15:35:07 +0000

    page_rights for role_detail implemented

--- lib/model/RightPeer.php
+++ lib/model/RightPeer.php
@@ -11,5 +11,13 @@
  * @package model
  */ 
 class RightPeer extends BaseRightPeer {
+	
+	public static function rightWithUniqueValueExists($iPageId, $sRoleKey, $bIsInherited) {
+		$oCriteria = new Criteria();
+		$oCriteria->add(self::PAGE_ID, $iPageId);
+		$oCriteria->add(self::ROLE_KEY, $sRoleKey);
+		$oCriteria->add(self::IS_INHERITED, $bIsInherited);
+		return self::doCount($oCriteria) > 0;
+	}
 }
 
--- modules/widget/role_detail/RoleDetailWidgetModule.php
+++ modules/widget/role_detail/RoleDetailWidgetModule.php
@@ -5,6 +5,7 @@
 class RoleDetailWidgetModule extends PersistentWidgetModule {
 
 	private $sRoleId = null;
+	private $oRole = null;
 	
 	public function setRoleId($sRoleId) {
 		$this->sRoleId = $sRoleId;
@@ -38,19 +39,49 @@ class RoleDetailWidgetModule extends PersistentWidgetModule {
 		$oFlash->checkForValue('role_key', 'role_key_required');
 		$oFlash->finishReporting();
 	}
+	
 
 	public function saveData($aRoleData) {
+		// ErrorHandler::log($aRoleData);
 		if($this->sRoleId === null) {
-			$oRole = new Role();
+			$this->oRole = new Role();
 		} else {
-			$oRole = RolePeer::retrieveByPK($this->sRoleId);
+			$this->oRole = RolePeer::retrieveByPK($this->sRoleId);
 		}
 		$this->validate($aRoleData);
 		if(!Flash::noErrors()) {
 			throw new ValidationException();
 		}
-		$oRole->setRoleKey($aRoleData['role_key']);
-		$oRole->setDescription($aRoleData['description']);
-		return $oRole->save();
+		$this->oRole->setRoleKey($aRoleData['role_key']);
+		$this->oRole->setDescription($aRoleData['description']);
+		if(isset($aRoleData['page_id_'])) {
+			$oRight = new Right();
+			$oRight->setRoleKey($this->oRole->getRoleKey());
+			$this->updateRights($oRight, $aRoleData);
+		}
+		foreach($this->oRole->getRightsJoinPage() as $oRight) {
+			if(isset($aRoleData['page_id_'.$oRight->getId()]) && $aRoleData['page_id_'.$oRight->getId()] != null) {
+				$this->updateRights($oRight, $aRoleData);			
+			} else {
+				$oRight->delete();
+			}
+		}
+		return $this->oRole->save();
+	}
+	
+	private function updateRights($oRight, $aRoleData) {
+		$mRightId = $oRight->isNew() ? '' : $oRight->getId();
+		$oRight->setIsInherited(isset($aRoleData['is_inherited_'.$mRightId]));
+		$oRight->setPageId($aRoleData['page_id_'.$mRightId]);
+		if($oRight->isNew() 
+			&& RightPeer::rightWithUniqueValueExists($oRight->getPageId(), $oRight->getRoleKey(), $oRight->getIsInherited())) {
+				return false;
+		}
+		$oRight->setMayEditPageContents(isset($aRoleData['may_edit_page_contents_'.$mRightId]));
+		$oRight->setMayEditPageDetails(isset($aRoleData['may_edit_page_details_'.$mRightId]));
+		$oRight->setMayDelete(isset($aRoleData['may_delete_'.$mRightId]));
+		$oRight->setMayCreateChildren(isset($aRoleData['may_create_children_'.$mRightId]));
+		$oRight->setMayViewPage(isset($aRoleData['may_view_page_'.$mRightId]));
+		$this->oRole->addRight($oRight);
 	}
 }
