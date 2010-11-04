3e6a5b1085364a3f805a8482e0acdd35a0cd0baf
Enrique Erne <info@mild.ch>
2010-11-04 14:55:39 +0000

    EmptyFrontendModule updated

--- modules/frontend/empty/EmptyFrontendModule.php
+++ modules/frontend/empty/EmptyFrontendModule.php
@@ -3,21 +3,21 @@
  * @package modules.frontend
  */
  
-class EmptyFrontendModule extends FrontendModule {
+class EmptyFrontendModule extends FrontendModule implements WidgetBasedFrontendModule {
 
 	public function __construct($oLanguageObject, $aRequestPath = null) {
 		parent::__construct($oLanguageObject, $aRequestPath);
 	}
 
-	public function renderFrontend() {
-		return "";
+	public function widgetData() {
+		return '';	
 	}
-
-	public function renderBackend() {
-		return $this->constructTemplate('backend');
+	
+	public function widgetSave($mData) {
+		return $this->oLanguageObject->setData('')->save();
 	}
-
-	public function getSaveData() {
-		return "";
+	
+	public function getWidget() {
+		return new GenericFrontendModuleWidgetModule(null, this, StringPeer::getString('widget.empty.confirm'));
 	}
 }
