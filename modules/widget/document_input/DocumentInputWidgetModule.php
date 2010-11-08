9438f6d8b72a2e52dbe0436736b6e93446ced211
Jürg Messmer <jmessmer@bluewin.ch>
2010-11-08 16:59:50 +0000

    detail media_object_edit

--- modules/widget/document_input/DocumentInputWidgetModule.php
+++ modules/widget/document_input/DocumentInputWidgetModule.php
@@ -3,6 +3,17 @@ class DocumentInputWidgetModule extends PersistentWidgetModule {
 	
 	public function getDocuments() {
 		$aResult = array();
+		// find files in media dirs - large files that cannot be uploaded with http
+		$aCustomFiles = array();
+		$aMediaDirs = ResourceFinder::findAllResources(array('web', 'media'));
+		foreach($aMediaDirs as $sAbsolutePath) {
+			self::readDirectory($sAbsolutePath, $aCustomFiles);
+		}
+		if(count($aCustomFiles) > 0) {
+			$sCustomFiles = StringPeer::getString('widget.documents.custom_files');
+			$aResult[$sCustomFiles] = $aCustomFiles;
+		}
+		// find files in database ordered by category
 		$aCategories = DocumentCategoryPeer::getDocumentCategoriesSorted();
 		foreach($aCategories as $oCategory) {
 			$aDocuments = DocumentPeer::getDocumentsByCategory($oCategory->getId());
@@ -15,6 +26,25 @@ class DocumentInputWidgetModule extends PersistentWidgetModule {
 			$aResult[$sWithoutCategory][$oDocument->getId()] = $oDocument->getName();
 		}
 		return $aResult;
+	}
+	
+	private static function readDirectory($sDir, &$aResult) {
+		$rDir = opendir($sDir);
+		if($rDir) {
+			while(($sFile = readdir($rDir)) !== false) {
+				$sFilePath = $sDir.DIRECTORY_SEPARATOR.$sFile;
+				if(StringUtil::startsWith($sFile, '.')) {
+					continue;
+				}
+				if(is_dir($sFilePath)) {
+					self::readDirectory($sFilePath, $aResult);
+				} else {
+					$oPath = new FileResource($sFilePath);
+					$aResult[$oPath->getRelativePath()] = $oPath->getFrontendPath();
+				}
+			}
+			closedir($rDir);
+		}
 	}
 
 	private static function getDocumentWithoutCategoryId() {
