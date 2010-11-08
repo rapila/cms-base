2cf0c9446b286317fc0b36d889f0362f09854ce8
Jürg Messmer <jmessmer@bluewin.ch>
2010-11-08 14:10:03 +0000

    document_input widget added with document_categories as optgroups

---
+++ modules/widget/document_input/DocumentInputWidgetModule.php
@@ -0,0 +1,26 @@
+<?php
+class DocumentInputWidgetModule extends PersistentWidgetModule {
+	
+	public function getDocuments() {
+		$aResult = array();
+		$aCategories = DocumentCategoryPeer::getDocumentCategoriesSorted();
+		foreach($aCategories as $oCategory) {
+			$aDocuments = DocumentPeer::getDocumentsByCategory($oCategory->getId());
+			foreach($aDocuments as $oDocument) {
+				$aResult[$oCategory->getName()][$oDocument->getId()] = $oDocument->getName();
+			}
+		}
+		$sWithoutCategory = StringPeer::getString('widget.documents.select_without_title');
+		foreach(self::getDocumentWithoutCategoryId() as $oDocument) {
+			$aResult[$sWithoutCategory][$oDocument->getId()] = $oDocument->getName();
+		}
+		return $aResult;
+	}
+
+	private static function getDocumentWithoutCategoryId() {
+		$oCriteria = new Criteria();
+		$oCriteria->add(DocumentPeer::DOCUMENT_CATEGORY_ID, null, Criteria::ISNULL);
+		$oCriteria->addAscendingOrderByColumn(DocumentPeer::NAME);
+		return DocumentPeer::doSelect($oCriteria);
+	}
+}
