53227c1a7ef32c7a9e86e97b5424ad097090bb76
Jürg Messmer <jmessmer@bluewin.ch>
2010-11-10 14:46:20 +0000

    login_edit widget module added

---
+++ modules/widget/login_edit/LoginEditWidgetModule.php
@@ -0,0 +1,16 @@
+<?php
+class LoginEditWidgetModule extends EditWidgetModule {
+
+	public function __construct($sSessionKey, $oFrontendModule) {
+		parent::__construct($sSessionKey, $oFrontendModule);
+	}
+
+	public function getDisplayOptions() {
+		$aResult = array();
+		foreach(LoginFrontendModule::$DISPLAY_OPTIONS as $sDisplayOption) {
+			$aResult[$sDisplayOption] = StringPeer::getString('option.'.$sDisplayOption);
+		}
+		return $aResult;
+	}
+	
+}
