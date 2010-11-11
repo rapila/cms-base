5eca41787b507634e88b6b2cfd62c0d3d38a036a
Jürg Messmer <jmessmer@bluewin.ch>
2010-11-11 11:18:50 +0000

    static getTaggedModels for usage in TagsAdminModule too

--- modules/widget/tag_edit/TagEditWidgetModule.php
+++ modules/widget/tag_edit/TagEditWidgetModule.php
@@ -33,10 +33,30 @@ class TagEditWidgetModule extends PersistentWidgetModule {
 		foreach(TagPeer::doSelect(new Criteria()) as $oTag) {
 			$aResult['tags'] = $oTag->getName();
 		}
-		$aResult['types'] = array();
+		$aResult['types'] = self::getTaggedModels();
 		foreach(TagFrontendModule::$DISPLAY_OPTIONS as $sName) {
       $aResult['types'][$sName] = StringPeer::getString('module.backend.'.$sName);
     }
+		return $aResult;
+	}
+	
+ /** getTaggedModels()
+	* to be used in TagsAdminModule
+	*/
+	public static function getTaggedModels($bCount=false) {
+		$oCriteria = new Criteria();
+		$oCriteria->clearSelectColumns()->addSelectColumn(TagInstancePeer::MODEL_NAME);
+		$oCriteria->setDistinct();
+		if($bCount) {
+			return TagInstancePeer::doCount($oCriteria);
+		}
+		$oCriteria->addAscendingOrderByColumn(TagInstancePeer::MODEL_NAME);
+		$aResult = array();
+		foreach(TagInstancePeer::doSelect($oCriteria) as $oInstance) {
+			$sTableName = constant($oInstance->getModelName().'Peer::TABLE_NAME');
+			$sName = StringPeer::getString('module.backend.'.$sTableName);
+			$aResult[$oInstance->getModelName()] = $sName;
+		}
 		return $aResult;
 	}
 	
