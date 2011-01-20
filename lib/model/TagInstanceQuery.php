d26eb633bc21815494e108a734e844c9faa478db
Raphael Schweikert <any@sabberworm.com>
2011-01-20 14:20:28 +0100

    fixed deletion of taginstances

--- lib/model/TagInstanceQuery.php
+++ lib/model/TagInstanceQuery.php
@@ -13,5 +13,9 @@
  * @package    propel.generator.model
  */
 class TagInstanceQuery extends BaseTagInstanceQuery {
-
+	public function filterByTagName($sTagName) {
+		$this->innerJoinTag();
+		$this->add(TagPeer::NAME, $sTagName);
+		return $this;
+	}
 } // TagInstanceQuery
