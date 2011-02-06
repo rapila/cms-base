797fcb0ee19ab575664f3322fc383fd79a19756a
Raphael Schweikert <any@sabberworm.com>
2011-02-06 18:13:17 +0100

    removed invalid test

--- lib/tests/StringPeerTests.php
+++ lib/tests/StringPeerTests.php
@@ -9,7 +9,4 @@ class StringPeerTests extends PHPUnit_Framework_TestCase {
   public function testTemplateString() {
     $this->assertSame("Tags", StringPeer::getString("module.admin.tags", "de"));
   }
-  public function testReplacedTemplateString() {
-    $this->assertSame("[vor] TEst", StringPeer::getString("page.sort_before", "de", null, array("name" => "TEst")));
-  }
 }
