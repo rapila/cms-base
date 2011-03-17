35074e274f3b53d58cbe805c16856701083b9246
Raphael Schweikert <any@sabberworm.com>
2011-03-17 18:20:42 +0100

    initial methods

--- modules/widget/dashboard_control/DashboardControlWidgetModule.php
+++ modules/widget/dashboard_control/DashboardControlWidgetModule.php
@@ -2,4 +2,38 @@
 
 class DashboardControlWidgetModule extends WidgetModule {
 	
+	public function allDashboardModules() {
+		$oUser = Session::getSession()->getUser();
+		$aDashboardConfig = $oUser->getAdminSettings('dashboard');
+		$aWidgets = array();
+		if(isset($aDashboardConfig['widgets'])) {
+			$aWidgets = &$aDashboardConfig['widgets'];
+		}
+		$bDidAddIds = false;
+		foreach($aWidgets as &$aWidget) {
+			if(!isset($aWidget['uid'])) {
+				$bDidAddIds = true;
+				$aWidget['uid'] = Util::uuid();
+			}
+		}
+		if($bDidAddIds) {
+			$oUser->setAdminSettings('dashboard', $aDashboardConfig);
+			$oUser->save();
+		}
+		return $aWidgets;
+	}
+	
+	public function template() {
+		$oUser = Session::getSession()->getUser();
+		$aDashboardConfig = $oUser->getAdminSettings('dashboard');
+		$sLayoutName = '1column';
+		if(isset($aDashboardConfig['layout'])) {
+			$sLayoutName = $aDashboardConfig['layout'];
+		}
+		return Module::constructTemplateForModuleAndType(AdminModule::getType(), AdminModule::getNameByClassName('DashboardAdminModule'), 'layouts/'.$sLayoutName)->render();
+	}
+	
+	public function saveConfig($aConfig) {
+		
+	}
 }
