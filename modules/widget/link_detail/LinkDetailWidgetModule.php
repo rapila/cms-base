8bfa059fe200d52ae8db789f3d247b527bc4fc1e
Jürg Messmer <jmessmer@bluewin.ch>
2011-04-07 09:58:55 +0200

    FIXED: language_id to NULL

--- modules/widget/link_detail/LinkDetailWidgetModule.php
+++ modules/widget/link_detail/LinkDetailWidgetModule.php
@@ -39,7 +39,7 @@ class LinkDetailWidgetModule extends PersistentWidgetModule {
 		$oLink->setName($aLinkData['name']);
 		$oLink->setLinkCategoryId($aLinkData['link_category_id'] == null ? null : $aLinkData['link_category_id']);
 		$oLink->setDescription($aLinkData['description']);
-		$oLink->setLanguageId($aLinkData['language_id']);
+		$oLink->setLanguageId($aLinkData['language_id'] != null ? $aLinkData['language_id'] : null);
 		$this->validate($aLinkData);
 		if(!Flash::noErrors()) {
 			throw new ValidationException();
