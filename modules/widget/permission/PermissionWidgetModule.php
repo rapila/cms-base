a7c11ec9a546c7b5810fb300b8e773db7f4f7e15
Raphael Schweikert <any@sabberworm.com>
2011-04-21 11:54:06 +0200

    implemented rudimentary permission widget

--- modules/widget/permission/PermissionWidgetModule.php
+++ modules/widget/permission/PermissionWidgetModule.php
@@ -2,19 +2,57 @@
 /**
  * @package modules.widget
  */
-class PermissionWidgetModule extends PersistentWidgetModule {
-
-	public function isGranted($sModuleName, $aSomeParameters) {
-		// may do something on page
-		// check may admin module, 
-		// modules decide for themselves?
-		if($isNotGranted) {
-			throw new LocalizedException('flash.whatever');
-		}
+class PermissionWidgetModule extends WidgetModule {
+	public static function moduleIsGranted($sModuleName, $sModuleType = 'widget') {
+		Module::isModuleAllowed($sModuleType, $sModuleName, Session::getSession()->getUser());
 	}
 	
-	public function getMayEditPage($iPageId) {
-		return true;
+	public static function userHasRole($sRoleKey) {
+		if(!Session::getSession()->isAuthenticated()) {
+			return false;
+		}
+		$oUser = Session::getSession()->getUser();
+		return $oUser->hasRole($sRoleKey);
+	}
+	
+	public function mayEditPageDetails($iPageId) {
+		if(!Session::getSession()->isAuthenticated()) {
+			return false;
+		}
+		$oUser = Session::getSession()->getUser();
+		return $oUser->mayEditPageDetails($iPageId);
+	}
+
+	public function mayEditPageContents($iPageId) {
+		if(!Session::getSession()->isAuthenticated()) {
+			return false;
+		}
+		$oUser = Session::getSession()->getUser();
+		return $oUser->mayEditPageContents($iPageId);
+	}
+
+	public function mayCreateChildren($iPageId) {
+		if(!Session::getSession()->isAuthenticated()) {
+			return false;
+		}
+		$oUser = Session::getSession()->getUser();
+		return $oUser->mayCreateChildren($iPageId);
+	}
+
+	public function mayDelete($iPageId) {
+		if(!Session::getSession()->isAuthenticated()) {
+			return false;
+		}
+		$oUser = Session::getSession()->getUser();
+		return $oUser->mayDelete($iPageId);
+	}
+
+	public function mayViewPage($iPageId) {
+		if(!Session::getSession()->isAuthenticated()) {
+			return false;
+		}
+		$oUser = Session::getSession()->getUser();
+		return $oUser->mayViewPage($iPageId);
 	}
 	
 	public static function isSingleton() {
