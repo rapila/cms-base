4f99d5b0195cdd85018b9522442c1ff838969934
Raphael Schweikert <any@sabberworm.com>
2011-05-05 19:49:45 +0200

    made delegated language init from AdminManager to initializeFirstUserIfEmpty

--- lib/classes/AdminManager.php
+++ lib/classes/AdminManager.php
@@ -46,13 +46,6 @@ class AdminManager extends Manager {
     if($sLanguageId === null) {
       $sLanguageId = Session::language();
     }
-    // hack that should be moved to a (potential) installManager
-    if(!LanguagePeer::languageExists($sLanguageId) && LanguagePeer::hasNoLanguage()) {
-      $oLanguage = new Language();
-      $oLanguage->setId($sLanguageId);
-      $oLanguage->setIsActive(true);
-      $oLanguage->save();
-    }
     return $sLanguageId;
   }
 
