cbced5b7a45f1c930db9a9d7c67dd0ea3dc2adf2
Raphael Schweikert <any@sabberworm.com>
2011-06-22 14:09:06 +0200

    added phpdoc comment

--- lib/model/Right.php
+++ lib/model/Right.php
@@ -7,6 +7,11 @@ require_once 'model/om/BaseRight.php';
  * @package model
  */ 
 class Right extends BaseRight {
+	
+	/**
+	* Returns whether the right allows editing the page structure (creating, moving and deleting objects).
+	* Currently aliased to may_edit_page_contents
+	*/
 	public function getMayEditPageStructure() {
 		return $this->getMayEditPageContents();
 	}
