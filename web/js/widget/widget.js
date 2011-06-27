e1766605f2c68d640b5d83e6a2a0726f6d9ccc9a
Raphael Schweikert <any@sabberworm.com>
2011-06-27 18:51:18 +0200

    allowed jQuery.Deferreds for most callbacks

--- web/js/widget/widget.js
+++ web/js/widget/widget.js
@@ -84,7 +84,7 @@ jQuery.extend(Widget.prototype, {
 			options = new WidgetJSONOptions();
 		}
 		var callback = args.pop();
-		if(!jQuery.isFunction(callback)) {
+		if(!callback || (!jQuery.isFunction(callback) && !callback.resolveWith)) {
 			callback !== undefined && args.push(callback);
 			callback = Widget.defaultMethodHandler;
 		}
@@ -103,12 +103,19 @@ jQuery.extend(Widget.prototype, {
 			options.async = !((name.indexOf('get') === 0 || name.indexOf('set') === 0) && (/[A-Z]/).test(name[3]));
 		}
 		if(options.callback_handles_error === null) {
-			options.callback_handles_error = callback.length>=2;
+			options.callback_handles_error = !!callback.resolveWith || callback.length>=2;
 		}
 		var action = options.action || 'methodCall';
 		var widget = this;
 		this._widgetJSON([action, name], function(response, exception) {
-			callback.call(widget, response.result, exception);
+			if(callback.resolveWith) {
+				if(exception) {
+					callback.rejectWith(widget, [exception]);
+				}
+				callback.resolveWith(widget, [response.result]);
+			} else {
+				callback.call(widget, response.result, exception);
+			}
 			result = response.result;
 			error = exception;
 		}, options, params);
@@ -268,7 +275,7 @@ jQuery.extend(Widget, {
 					}
 					options.action = 'staticMethodCall';
 					var callback = args.pop();
-					if(!jQuery.isFunction(callback)) {
+					if(!callback || (!jQuery.isFunction(callback) && !callback.resolveWith)) {
 						callback !== undefined && args.push(callback);
 						callback = Widget.defaultMethodHandler;
 					}
@@ -296,7 +303,7 @@ jQuery.extend(Widget, {
 	
 	create: function(widgetType, finishCallback, session) {
 		var intermediateCallback = jQuery.noop;
-		if(jQuery.isFunction(session)) {
+		if(session && (jQuery.isFunction(session) || session.resolveWith)) {
 			//intermediate callback given → shift session
 			intermediateCallback = finishCallback;
 			finishCallback = session;
@@ -304,10 +311,18 @@ jQuery.extend(Widget, {
 		}
 		if(Widget.singletons[widgetType]) {
 			if(intermediateCallback) {
-				intermediateCallback(Widget.singletons[widgetType]);
+				if(intermediateCallback.resolveWith) {
+					intermediateCallback.resolve(Widget.singletons[widgetType]);
+				} else {
+					intermediateCallback(Widget.singletons[widgetType]);
+				}
 			}
 			if(finishCallback) {
-				finishCallback(Widget.singletons[widgetType]);
+				if(finishCallback.resolveWith) {
+					finishCallback.resolve(Widget.singletons[widgetType]);
+				} else {
+					finishCallback(Widget.singletons[widgetType]);
+				}
 			}
 			return Widget.singletons[widgetType];
 		}
@@ -332,13 +347,21 @@ jQuery.extend(Widget, {
 				Widget.singletons[widgetType] = widget;
 			}
 			if(intermediateCallback) {
-				intermediateCallback(widget);
+				if(intermediateCallback.resolveWith) {
+					intermediateCallback.resolve(widget);
+				} else {
+					intermediateCallback(widget);
+				}
 			}
 			if(widget.initialize) {
 				widget.initialize();
 			}
 			if(finishCallback) {
-				finishCallback(widget);
+				if(finishCallback.resolveWith) {
+					finishCallback.resolve(widget);
+				} else {
+					finishCallback(widget);
+				}
 			}
 			if(widget.prepare) {
 				widget.prepare();
@@ -352,7 +375,7 @@ jQuery.extend(Widget, {
 	
 	createWithElement: function(widgetType, finishCallback, session) {
 		var intermediateCallback = jQuery.noop;
-		if(jQuery.isFunction(session)) {
+		if(session && (jQuery.isFunction(session) || session.resolveWith)) {
 			//intermediate callback given → shift session
 			intermediateCallback = finishCallback;
 			finishCallback = session;
@@ -362,7 +385,11 @@ jQuery.extend(Widget, {
 			widget._element = jQuery.parseHTML(widget._instanceInformation.content);
 			widget.fire('element_set', widget._element);
 			widget.handle('prepared', function(event, widget) {
-				finishCallback(widget);
+				if(finishCallback.resolveWith) {
+					finishCallback.resolve(widget);
+				} else {
+					finishCallback(widget);
+				}
 			}, false);
 		}, session);
 	},
@@ -452,7 +479,7 @@ jQuery.extend(Widget, {
 			}
 		}
 		if(options.callback_handles_error === null) {
-			options.callback_handles_error = callback.length>=2;
+			options.callback_handles_error = !!callback.resolveWith || callback.length>=2;
 		}
 		var ajaxOpts = {
 			url: url,
@@ -472,7 +499,7 @@ jQuery.extend(Widget, {
 					xmlhttprequest.upload.addEventListener('progress', options.upload_progess_callback, false);
 				}
 				if(options.send_as_binary && xmlhttprequest.sendAsBinary) {
-					xmlhttprequest.send = xmlhttprequest.sendAsBinary
+					xmlhttprequest.send = xmlhttprequest.sendAsBinary;
 				}
 				return xmlhttprequest;
 			},
@@ -490,7 +517,14 @@ jQuery.extend(Widget, {
 					call_callback = options.callback_handles_error || exception_handler(error, widgetType, widgetOrId, action, callback, options, attributes);
 				}
 				if(call_callback) {
-					callback.call(this, result, error);
+					if(callback.resolveWith) {
+						if(error) {
+							callback.rejectWith(this, [error]);
+						}
+						callback.resolveWith(this, [result]);
+					} else {
+						callback.call(this, result, error);
+					}
 				}
 			},
 			error: function(request, statusCode, error) {
@@ -502,7 +536,11 @@ jQuery.extend(Widget, {
 				var exception_handler = Widget.exception_type_handlers[error_object.exception_type] || Widget.exception_type_handlers.fallback;
 				action.shift();
 				if(options.callback_handles_error || exception_handler(error_object, widgetType, widgetOrId, action, callback, options, attributes)) {
-					callback.call(this, {}, error_object);
+					if(callback.resolveWith) {
+						callback.rejectWith(this, [error_object]);
+					} else {
+						callback.call(this, {}, error_object);
+					}
 				}
 			},
 			complete: function() {
