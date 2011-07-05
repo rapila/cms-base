cde0eadcd2523b145f82d4123b6b46067e38cb88
Raphael Schweikert <any@sabberworm.com>
2011-07-05 12:19:11 +0200

    removed text/plain headers on Util::dumpAll if xdebug has replaced the var_dump function to return html

--- lib/classes/Util.php
+++ lib/classes/Util.php
@@ -99,7 +99,9 @@ class Util {
 	public static function dumpAll() {
 		$aArgs = func_get_args();
 		@ob_clean();
-		header("Content-Type: text/plain;charset=".Settings::getSetting('encoding', 'browser', 'utf-8'));
+		if(!function_exists('xdebug_break')) {
+			header("Content-Type: text/plain;charset=".Settings::getSetting('encoding', 'browser', 'utf-8'));
+		}
 		call_user_func_array('var_dump', $aArgs);
 		exit();
 	}
