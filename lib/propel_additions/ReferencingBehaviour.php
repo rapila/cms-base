1bb7155741f8fe17bfe7f16b42334008238af5f2
Raphael Schweikert <any@sabberworm.com>
2011-09-12 10:58:18 +0200

    changed referencing behaviour to also work if ModelPeer::doDelete() was used

--- lib/propel_additions/ReferencingBehaviour.php
+++ lib/propel_additions/ReferencingBehaviour.php
@@ -7,14 +7,40 @@
  */
 class ReferencingBehaviour extends Behavior
 {
-	/**
-	 * Add code in ObjectBuilder::preUpdate
-	 *
-	 * @return    string The code to put at the hook
-	 */
-	public function postDelete()
-	{
-		return "ReferencePeer::removeReferences(\$this);";
+	public function staticMethods($oBuilder) {
+		$oBuilder->declareClassFromBuilder($oBuilder->getStubQueryBuilder());
+		return "public static function doDelete2(\$values, PropelPDO \$con = null) {
+		if (\$con === null) {
+			\$con = Propel::getConnection(PagePeer::DATABASE_NAME, Propel::CONNECTION_WRITE);
+		}
+
+		if(\$values instanceof Criteria) {
+			// rename for clarity
+			\$criteria = clone \$values;
+		} elseif (\$values instanceof {$this->getTable()->getPhpName()}) { // it's a model object
+			// create criteria based on pk values
+			\$criteria = \$values->buildPkeyCriteria();
+		} else { // it's a primary key, or an array of pks
+			\$criteria = new Criteria(self::DATABASE_NAME);
+			\$criteria->add(PagePeer::ID, (array) \$values, Criteria::IN);
+		}
+		
+		foreach(PagePeer::doSelect(\$criteria, \$con) as \$object) {
+			ReferencePeer::removeReferences(\$this);
+		}
+
+		return self::originalDoDelete(\$criteria, \$con);
+}";
+	}
+
+	public function peerFilter(&$sScript) {
+		$sScript = str_replace(array(
+			'public static function doDelete(', 
+			'public static function doDelete2('
+		), array(
+			'private static function originalDoDelete(',
+			'public static function doDelete('
+		), $sScript);
 	}
 
 	public function objectMethods($builder)
