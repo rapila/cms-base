86fe3f0c0a7126dcfb5e6371a1833730c0000825
Raphael Schweikert <any@sabberworm.com>
2011-10-19 22:09:23 +0200

    Added draft status support
    All language object may now exist in draft status, even if they don’t yet exist. Draft objects can only be created in admin but are visible in preview. When saving a draft item in preview, it gets to be published.
    Publishing draft items makes a backup copy of the last object configuration (though there is not yet any UI to retrieve old objects).

--- lib/model/LanguageObjectHistory.php
+++ lib/model/LanguageObjectHistory.php
@@ -7,17 +7,34 @@ require_once 'model/om/BaseLanguageObjectHistory.php';
  * @package		 model
  */
 class LanguageObjectHistory extends BaseLanguageObjectHistory {
+	/**
+	* Corresponds to the overriding of {@link LanguageObjectPeer::retrieveByPK()}
+	* Provides a unified way of working with stored references (in the references or tags tables)
+	*/
+	public function getId() {
+		return $this->getObjectId().'_'.$this->getLanguageId().'_'.$this->getRevision();
+	}
 
 	public function getName() {
 		return 'Backup '.$this->getRevision().', '.$this->getCreatedAt('Y-m-d').' / '.$this->getUser()->getInitials();
 	}
+
+	public function preInsert(PropelPDO $con = null) {
+		$this->setRevision($this->getNextRevision());
+		return true;
+	}
 	
 	private function getNextRevision() {
-		$oLanguageObjectHistory = LanguageObjectHistoryPeer::doSelectOne(LanguageObjectHistoryPeer::getHistoryByObjectAndLanguageIdCriteria($this->getObjectId(), $this->getLanguageId()));
+		$oLanguageObjectHistory = LanguageObjectHistoryQuery::create()->filterByLanguageObject($this)->sort()->findOne();
 		if($oLanguageObjectHistory !== null) {
 			return ($oLanguageObjectHistory->getRevision()+1);
 		}
 		return 1;
+	}
+
+	///Convenience function to allow LanguageObjectHistory objects in contexts where LanguageObject objects are expected
+	public function getDraft() {
+		return $this;
 	}
 }
 
--- lib/model/LanguageObjectHistoryQuery.php
+++ lib/model/LanguageObjectHistoryQuery.php
@@ -13,5 +13,14 @@
  * @package    propel.generator.model
  */
 class LanguageObjectHistoryQuery extends BaseLanguageObjectHistoryQuery {
+	public function filterByLanguageObject($oLanguageObject) {
+		$this->filterByObjectId($oLanguageObject->getObjectId());
+		$this->filterByLanguageId($oLanguageObject->getLanguageId());
+		return $this;
+	}
+
+	public function sort() {
+		return $this->orderByRevision(Criteria::DESC);
+	}
 
 } // LanguageObjectHistoryQuery
