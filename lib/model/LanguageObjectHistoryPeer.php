b81b719198a310207e81d365d4cdb9c336bdd027
Jürg Messmer <jm@mosaics.ch>
2011-12-05 12:54:50 +0100

    old methods removed

--- lib/model/LanguageObjectHistoryPeer.php
+++ lib/model/LanguageObjectHistoryPeer.php
@@ -12,24 +12,6 @@
  */
 class LanguageObjectHistoryPeer extends BaseLanguageObjectHistoryPeer {
 
-	public static function getHistoryByLanguageObject(LanguageObject $oLanguageObject, $iLimit=10) {
-		return self::getHistoryByObjectAndLanguageId($oLanguageObject->getObjectId(), $oLanguageObject->getLanguageId(), $iLimit);
-	}
-	
-	public static function getHistoryByObjectAndLanguageId($iObjectId, $sLanguageId, $iLimit=10) {
-		$oCriteria = self::getHistoryByObjectAndLanguageIdCriteria($iObjectId, $sLanguageId);
-		$oCriteria->setLimit($iLimit);
-		return self::doSelect($oCriteria);
-	}
-	
-	public static function getHistoryByObjectAndLanguageIdCriteria($iObjectId, $sLanguageId, $iLimit=10) {
-		$oCriteria = new Criteria();
-		$oCriteria->add(self::OBJECT_ID, $iObjectId);
-		$oCriteria->add(self::LANGUAGE_ID, $sLanguageId);
-		$oCriteria->addDescendingOrderByColumn(self::REVISION);
-		return $oCriteria;
-	}
-	
 	public static function mayOperateOn($oUser, $oLanguageObjectHistory, $sOperation) {
 		if($oUser === null || !$oUser->getIsBackendLoginEnabled()) {
 			return false;
