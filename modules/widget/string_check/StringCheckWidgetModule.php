5bec0e44e7f1610a049af19029734059ae62a983
Jürg Messmer <jm@mosaics.ch>
2011-12-12 17:57:04 +0100

    checkStrings added

--- modules/widget/string_check/StringCheckWidgetModule.php
+++ modules/widget/string_check/StringCheckWidgetModule.php
@@ -5,7 +5,9 @@
 class StringCheckWidgetModule extends PersistentWidgetModule {
 
 	private $aLogMessages = array();
-	private static $CHECK_OPTIONS = array("static_strings");
+	private static $CHECK_OPTIONS = array("static_strings", "strings");
+
+	private static $EMPTY_STRING_KEY = "139c4e89cdbedaf144d05ca54a12a57b";
 
 	const LOG_LEVEL_INFO = 0;
 	const LOG_LEVEL_NOTICE = 2;
@@ -19,19 +21,48 @@ class StringCheckWidgetModule extends PersistentWidgetModule {
 	public function checkOptions() {
 		$aResult = array();
 		foreach(self::$CHECK_OPTIONS as $sCheckOption) {
+			// no string check required if there is no second language
+			if($sCheckOption === 'strings' && LanguageQuery::create()->count() === 1) {
+				continue;
+			}
 			$aResult[$sCheckOption] = StringPeer::getString('check_option.'.$sCheckOption, null, StringUtil::makeReadableName($sCheckOption));
 		}
 		return $aResult;
 	}
 	
 	public function check($sCheckName, $sLanguageId = null) {
+		$this->aLogMessages = array();
 		switch($sCheckName) {
 			case "static_strings":
 				return $this->checkStaticStrings($sLanguageId);
 			break;
+			case "strings":
+				return $this->checkStrings($sLanguageId);
+			break;
 		}
 	}
 	
+	private function checkStrings($sCheckLanguageId = null) {
+		$aAllStringObjects = StringQuery::create()->orderByStringKey()->find();
+		$aAllStrings = array();
+		$aAllLanguages = LanguagePeer::getLanguagesAssoc();
+		foreach($aAllStringObjects as $oString) {
+			if(!in_array($oString->getStringKey(), $aAllStrings))
+				$aAllStrings[] = $oString->getStringKey();
+		}
+
+		foreach($aAllStrings as $sStringKey) {
+			foreach($aAllLanguages as $sLanguageId => $sLanguageName) {
+				$oString = StringPeer::getString($sStringKey, $sLanguageId, self::$EMPTY_STRING_KEY);
+				if($oString === self::$EMPTY_STRING_KEY) {
+					$sText = StringPeer::getString('wns.check.check_string_message', null, null, array('string_key' => $sStringKey, 'language_id' => $sLanguageId));
+					$this->log($sText, $sLanguageId, self::LOG_LEVEL_WARNING);
+				}
+			}
+		}
+		return $this->aLogMessages;
+	}
+
 	private function checkStaticStrings($sCheckLanguageId = null) {
 		$aLanguageFiles = array_merge(ResourceFinder::findResourceByExpressions(array(DIRNAME_LANG, "/^.+\.ini$/"), ResourceFinder::SEARCH_SITE_FIRST), ResourceFinder::findResourceByExpressions(array(DIRNAME_MODULES, ResourceFinder::ANY_NAME_OR_TYPE_PATTERN, ResourceFinder::ANY_NAME_OR_TYPE_PATTERN, DIRNAME_LANG, "/^.+\.ini$/"), ResourceFinder::SEARCH_SITE_FIRST));
 		
