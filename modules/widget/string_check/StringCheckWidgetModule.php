3ae3fdb04412f01beb816ea7834270e7d519a360
Jürg Messmer <jm@mosaics.ch>
2011-12-12 14:42:06 +0100

    language option added
    check only one language possible
    missing strings added

--- modules/widget/string_check/StringCheckWidgetModule.php
+++ modules/widget/string_check/StringCheckWidgetModule.php
@@ -5,7 +5,7 @@
 class StringCheckWidgetModule extends PersistentWidgetModule {
 
 	private $aLogMessages = array();
-	private static $CHECK_OPTIONS = array("static strings");
+	private static $CHECK_OPTIONS = array("static_strings");
 
 	const LOG_LEVEL_INFO = 0;
 	const LOG_LEVEL_NOTICE = 2;
@@ -17,34 +17,39 @@ class StringCheckWidgetModule extends PersistentWidgetModule {
 	}
 	
 	public function checkOptions() {
-		return self::$CHECK_OPTIONS;
+		$aResult = array();
+		foreach(self::$CHECK_OPTIONS as $sCheckOption) {
+			$aResult[$sCheckOption] = StringPeer::getString('check_option.'.$sCheckOption, null, StringUtil::makeReadableName($sCheckOption));
+		}
+		return $aResult;
 	}
 	
-	public function check($sCheckName) {
+	public function check($sCheckName, $sLanguageId = null) {
 		switch($sCheckName) {
-			case "static strings":
-				return $this->checkStaticStrings();
+			case "static_strings":
+				return $this->checkStaticStrings($sLanguageId);
 			break;
 		}
 	}
 	
-	private function checkStaticStrings() {
+	private function checkStaticStrings($sCheckLanguageId = null) {
 		$aLanguageFiles = array_merge(ResourceFinder::findResourceByExpressions(array(DIRNAME_LANG, "/^.+\.ini$/"), ResourceFinder::SEARCH_SITE_FIRST), ResourceFinder::findResourceByExpressions(array(DIRNAME_MODULES, ResourceFinder::ANY_NAME_OR_TYPE_PATTERN, ResourceFinder::ANY_NAME_OR_TYPE_PATTERN, DIRNAME_LANG, "/^.+\.ini$/"), ResourceFinder::SEARCH_SITE_FIRST));
 		
-		$aSortedLanugageFiles = array();
+		$aSortedLanguageFiles = array();
+		// for each lang dir the available language files are collected
 		foreach($aLanguageFiles as $sRelativePath => $sAbsolutePath) {
 			$aPathParts = array();
 			preg_match("/(([^\/]+\/)+)(.+)\.ini/", $sRelativePath, $aPathParts);
 			$sPathPrefix = $aPathParts[1];
 			$sLanguageId = $aPathParts[3];
-			if(!isset($aSortedLanugageFiles[$sPathPrefix])) {
-				$aSortedLanugageFiles[$sPathPrefix] = array();
+			if(!isset($aSortedLanguageFiles[$sPathPrefix])) {
+				$aSortedLanguageFiles[$sPathPrefix] = array();
 			}
-			$aSortedLanugageFiles[$sPathPrefix][] = $sLanguageId;
+			$aSortedLanguageFiles[$sPathPrefix][] = $sLanguageId;
 		}
-		
-		foreach($aSortedLanugageFiles as $sPathPrefix => $aLanguageIds) {
-			$this->log("Checking static strings in $sPathPrefix");
+		$aAllStringsAvailable = array();
+		foreach($aSortedLanguageFiles as $sPathPrefix => $aLanguageIds) {
+			$this->log(StringPeer::getString('wns.check.check_static_strings_title', null, null, array('path_prefix' => $sPathPrefix)));
 			$aAllStrings = array();
 			$aAllLanguageFileContents = array();
 			foreach($aLanguageIds as $sLanguageId) {
@@ -56,22 +61,31 @@ class StringCheckWidgetModule extends PersistentWidgetModule {
 				$aAllStrings = array_merge($aAllStrings, $aAllLanguageFileContents[$sLanguageId]);
 			}
 			$aAllStrings = array_keys($aAllStrings);
+			$bFileHasErrors = false;
 			foreach($aAllStrings as $sStringKey) {
 				foreach($aAllLanguageFileContents as $sLanguageId => $aLanguageFileContents) {
+					if($sCheckLanguageId !== null && $sLanguageId !== $sCheckLanguageId) {
+						continue;
+					}
 					if(!isset($aLanguageFileContents[$sStringKey])) {
-						$this->log("String $sStringKey does not exist in $sPathPrefix$sLanguageId.ini", self::LOG_LEVEL_WARNING);
+						$sText = StringPeer::getString('wns.check.check_message', null, null, array('string_key' => $sStringKey, 'file_name' => $sPathPrefix.$sLanguageId.'.ini'));
+						$this->log($sText, $sLanguageId, self::LOG_LEVEL_WARNING);
+						$bFileHasErrors = true;
 					}
 				}
+			}
+			if($bFileHasErrors === false) {
+				$this->log('ok!', null, 5);
 			}
 		}
 		return $this->aLogMessages;
 	}
 	
-	private function log($sText, $iLogLevel=0) {
-
+	private function log($sText, $sLanguageId=null, $iLogLevel=0) {
 		$aLog = array();
 		$aLog['level'] = $iLogLevel;
 		$aLog['message'] = $sText;
+		$aLog['language_id'] = $sLanguageId;
 		$this->aLogMessages[] = $aLog;
 	}
 }
