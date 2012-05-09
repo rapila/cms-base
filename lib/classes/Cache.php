fce018d03a2c825e20d748902da81626dc93bc6b
Raphael Schweikert <any@sabberworm.com>
2012-05-09 08:12:31 +0200

    get cache key instead of file name

--- lib/classes/Cache.php
+++ lib/classes/Cache.php
@@ -3,7 +3,7 @@
 class Cache {
 	
 	private $bCacheIsNeverOff;
-	private $sFileName;
+	private $sKey;
 	private $sFilePath;
 	private $bCacheControlHeaderSent;
 	
@@ -18,8 +18,8 @@ class Cache {
 			}
 		}
 		
-		$this->sFileName = md5($sKey);
-		$this->sFilePath = $sPath.'/'.$this->sFileName.'.cache';
+		$this->sKey = $sKey;
+		$this->sFilePath = $sPath.'/'.md5($sKey).'.cache';
 		
 		$this->bCacheControlHeaderSent = false;
 	}
@@ -42,10 +42,10 @@ class Cache {
 	}
 	
 	/**
-	 * Returns the md5()’ed cache key.
+	 * Returns the (not md5()’ed) cache key.
 	 */
-	public function getFileName() {
-			return $this->sFileName;
+	public function getKey() {
+			return $this->sKey;
 	}
 	
 	/**
