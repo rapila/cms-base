67443eb0f557b94b5c92706bdca61d206a4e4995
Jürg Messmer <jm@mosaics.ch>
2012-09-12 16:00:31 +0200

    fix indent for webkit and sort order in Chrome
    getTree replaced by recursive iterator

--- modules/widget/page_input/PageInputWidgetModule.php
+++ modules/widget/page_input/PageInputWidgetModule.php
@@ -5,11 +5,26 @@
 class PageInputWidgetModule extends WidgetModule {
 	
 	public function pages() {
-		return PagePeer::getRootPage()->getTree(true);
+		$aResult = array();
+		$cGather = function($oIterator, $iLevel) use(&$aResult, &$cGather) {
+			$oCurrentPage = null;
+			while(($oCurrentPage = $oIterator->current()) !== null) {
+				$oResult = new StdClass();
+				$oResult->level = $iLevel;
+				$oResult->page_id = $oCurrentPage->getId();
+				$oResult->name = $oCurrentPage->getName();
+				$aResult[] = $oResult;
+				if($oIterator->hasChildren()) {
+					$cGather($oIterator->getChildren(), $iLevel+1);
+				}
+				$oIterator->next();
+			}
+		};
+		$cGather(PagePeer::getRootPage()->getIterator(), 0);
+		return $aResult;
 	}
 	
   public function getElementType() {
 		return 'select';
 	}
-
 }
