a63bddcd9e95f3b394cbfcdab85493ab851df08d
Jürg Messmer <jm@mosaics.ch>
2012-09-12 17:38:28 +0200

    added: convenience method

--- lib/model/PagePropertyQuery.php
+++ lib/model/PagePropertyQuery.php
@@ -6,5 +6,8 @@
  */
 class PagePropertyQuery extends BasePagePropertyQuery {
 
+	public function filterBySplitValue($iJournalId, $sChar = ',') {
+		return $this->filterByValue($iJournalId)->_or()->filterByValue("$iJournalId$sChar%")->_or()->filterByValue("%$sChar$iJournalId$sChar%")->_or()->filterByValue("%$sChar$iJournalId");
+	}
 }
 
