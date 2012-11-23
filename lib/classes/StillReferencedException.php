0ce30a7d6c07a1a528df6457817eb964035d9846
Jürg Messmer <jm@mosaics.ch>
2012-11-23 14:54:51 +0100

    added fallback for better info if not language object

--- lib/classes/StillReferencedException.php
+++ lib/classes/StillReferencedException.php
@@ -6,19 +6,22 @@ class StillReferencedException extends LocalizedException {
 		foreach($oReferencedObject->getReferees() as $oReferee) {
 			$oFrom = $oReferee->getFrom();
 			$aFrom = array();
-			$aFrom["name"] = Util::descriptionForObject($oFrom, $sDefaultLanguageId);
 			$aFrom["model"] = $oReferee->getFromModelName();
+			$aFrom["name"] = Util::descriptionForObject($oFrom, $sDefaultLanguageId);
+			if(!$oFrom instanceof LanguageObject) {
+				$aFrom["name"] = $aFrom["model"].': '.$aFrom["name"];
+			}
+
 			if(method_exists($oFrom, 'getAdminWidget')) {
 				$oWidget = $oFrom->getAdminWidget();
 				$aFrom["admin_widget"] = array($oWidget->getModuleName(), $oWidget->getSessionKey());
 			} elseif(method_exists($oFrom, 'getAdminLink')) {
 				$aFrom["admin_link"] = LinkUtil::link($oFrom->getAdminLink(), 'AdminManager');
 			}
-			
+
 			$aMessageParameters['references'][] = $aFrom;
 		}
 		$aMessageParameters['to'] = Util::nameForObject($oReferencedObject);
-
 		parent::__construct('wns.still_referenced.message', $aMessageParameters, null, 0, $sDefaultLanguageId);
 	}
 }
