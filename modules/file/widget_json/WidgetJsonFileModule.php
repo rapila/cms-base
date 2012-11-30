190f78fea3c9602671516a8c4389b22bb0a1f6cf
Raphael Schweikert <any@sabberworm.com>
2012-11-30 09:39:22 +0100

    Complete partial merge in feacdc466998efc8d46504e6d502c87806e591b0

--- modules/file/widget_json/WidgetJsonFileModule.php
+++ modules/file/widget_json/WidgetJsonFileModule.php
@@ -77,21 +77,13 @@ class WidgetJsonFileModule extends FileModule {
 				throw new LocalizedException('wns.file.widget_json.method_does_not_exist', array('method' => $sMethodName, 'widget' => $sWidgetClass));
 			}
 			return array("result" => call_user_func_array(array($sWidgetClass, $sMethodName), isset($aRequest['method_parameters']) ? $aRequest['method_parameters'] : array()));
-		} 
-		$oWidget = null;
-		if(isset($aRequest['session_key'])) {
-			$oWidget = Session::getSession()->getArrayAttributeValueForKey(WidgetModule::WIDGET_SESSION_KEY, $aRequest['session_key']);
 		}
-		$bIsNew = $oWidget === null;
-		if($bIsNew) {
-			$aInstanceArgs = array(@$aRequest['session_key']);
-			if(isset($aRequest['instance_args'])) {
-				$aInstanceArgs = $aRequest['instance_args'];
-			}
-			$aNewArgs = array_merge(array($this->sWidgetType), $aInstanceArgs);
-			$oWidget = call_user_func_array(array('WidgetModule', 'getModuleInstance'), $aNewArgs);
+		$aInstanceArgs = array(@$aRequest['session_key']);
+		if(isset($aRequest['instance_args'])) {
+			$aInstanceArgs = $aRequest['instance_args'];
 		}
-		
+		$aNewArgs = array_merge(array($this->sWidgetType), $aInstanceArgs);
+		$oWidget = call_user_func_array(array('WidgetModule', 'getWidget'), $aNewArgs);
 		if($this->sAction === 'instanciateWidget') {
 			$this->checkPermissions($sWidgetClass);
 			$aInformation = array();
