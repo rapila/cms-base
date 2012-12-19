ccc68dad74c979300d505834dfd00a2abb39cc1f
Jürg Messmer <jm@mosaics.ch>
2012-12-19 11:43:59 +0100

    disable role_key input if role is used
    added convenience method filterByRole to UserGroupQuery

--- lib/model/UserGroupQuery.php
+++ lib/model/UserGroupQuery.php
@@ -5,6 +5,9 @@
  * @package    propel.generator.model
  */
 class UserGroupQuery extends BaseUserGroupQuery {
-
+	
+	public function filterByRole($oRole) {
+		return $this->useGroupQuery()->useGroupRoleQuery()->filterByRole($oRole)->endUse()->endUse();
+	}
 }
 
