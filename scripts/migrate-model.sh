6427b2543615cc325e1ed454c9795f2f94b37893
Raphael Schweikert <any@sabberworm.com>
2013-01-15 12:32:03 +0100

    Make migrations per-CONTEXT_DIR

--- scripts/migrate-model.sh
+++ scripts/migrate-model.sh
@@ -1,14 +1,42 @@
 #! /bin/bash
 
+shopt -s nullglob
+
 path_to_this="$(dirname "$0")";base_path="$(dirname "$path_to_this")";. "$base_path/lib/sh/script_common.sh"
 
 owner=`whoami`
 path_to_buildfile="base/lib/vendor/propel/generator/build.xml"
 action=migrate
 
+part=base
+
 if [ "$1" = "help" ]; then
-	echo "USAGE: migrate-model.sh [migrate|up|down|status (default migrate)] [<path-to-buildfile (default $path_to_buildfile)>] [<php-user (default `whoami`)>]"
+	echo "USAGE: migrate-model.sh [-b|-s|-p <plugin-name> (default -b)] [migrate|up|down|status (default migrate)] [<path-to-buildfile (default $path_to_buildfile)>] [<php-user (default `whoami`)>]"
+	echo "OR: migrate-model.sh migrate-all"
 	exit 0
+fi
+
+if [ "$1" = "migrate-all" ]; then
+	plugin_migrations="plugins/*"
+	for plugin in $plugin_migrations; do
+		plugin=${plugin/plugins\//}
+		("$0" -p "$plugin" migrate)
+	done
+	("$0" -b migrate)
+	("$0" -s migrate)
+	exit 0
+fi
+
+if [ "$1" = "-b" ]; then
+	part=base
+	shift
+elif [ "$1" = "-s" ]; then
+	part=site
+	shift
+elif [ "$1" = "-p" ]; then
+	shift
+	part="plugins/$1"
+	shift
 fi
 
 if [ "$1" = "up" ]; then
@@ -40,11 +68,11 @@ fi
 
 SUDO="sudo -u $owner env RAPILA_ENVIRONMENT=${RAPILA_ENVIRONMENT}"
 
-cp base/build.properties generated/
 mkdir -p "./generated/migrations"
 
-$SUDO "$PHP_PATH" -r "require_once('base/lib/inc.php');BuildHelper::preMigrate();BuildHelper::consolidateMigrations();"
-$SUDO /bin/sh "$PHING_PATH" -f "$path_to_buildfile" -Dproject.dir=generated/ "$action"
-$SUDO "$PHP_PATH" -r "require_once('base/lib/inc.php');BuildHelper::postMigrate();"
-rm "./generated/migrations/"*.php
+cp base/build.properties generated/ && \
+$SUDO "$PHP_PATH" -r "require_once('base/lib/inc.php');BuildHelper::preMigrate();BuildHelper::consolidateMigrations('$part');" && \
+$SUDO /bin/sh "$PHING_PATH" -f "$path_to_buildfile" -Dproject.dir=generated/ "-Dpropel.migration.table=propel_miration_${part/\//_}" "$action" && \
+$SUDO "$PHP_PATH" -r "require_once('base/lib/inc.php');BuildHelper::postMigrate();" && \
+rm "./generated/migrations/"*.php 2> /dev/null
 
