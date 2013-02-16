72235f20f5cf8342b4f2feac0451ae7e5df47a5e
Jürg Messmer <jm@mosaics.ch>
2013-02-16 11:35:07 +0100

    refactored peer methods to use query classes
    • deprecated old peer methods to dump asap
    • simplified and cleanups

--- lib/model/DocumentTypeQuery.php
+++ lib/model/DocumentTypeQuery.php
@@ -5,26 +5,25 @@
  * @package   propel.generator.model
  */
 class DocumentTypeQuery extends BaseDocumentTypeQuery {
-	public function filterByDocumentKind($sDocumentKind = 'image', $bInclude = true) {
-		$this->filterByMimetype("$sDocumentKind/%", $bInclude ? Criteria::LIKE : Criteria::NOT_LIKE);
+
+	public function filterByDocumentKind($sDocumentKind = 'image', $bLike = true) {
+		$this->filterByMimetype("$sDocumentKind/%", $bLike ? Criteria::LIKE : Criteria::NOT_LIKE);
 		return $this;
 	}
-	
+
 	public static function findDocumentTypeByMimetype($sMimetype=null) {
 		return self::create()->filterByMimeType($sMimetype)->findOne();
 	}
-	
+
 	public static function findDocumentTypeByExtension($sMimetype=null) {
 		return self::create()->filterByExtension($sMimetype)->findOne();
 	}
-	
+
 	public static function findDocumentTypeIDsByKind($sMimeTypeKind, $bLike = true) {
 		return self::create()->filterByDocumentKind($sMimeTypeKind, $bLike)->select(array('Id'))->find()->toArray();
 	}
-	
+
 	public static function findDocumentTypeAndMimetypeByDocumentKind($sMimeTypeKind='image', $bLike=true) {
 		return self::create()->filterByDocumentKind($sMimeTypeKind, $bLike)->select()->find()->toKeyValue('Id', 'Mimetype');
 	}
-	
 }
-
