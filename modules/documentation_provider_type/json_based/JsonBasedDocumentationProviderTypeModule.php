282457164d6de6e1bd64c79046702d47d9171d5d
Jürg Messmer <jm@mosaics.ch>
2013-03-07 08:25:16 +0100

    add documentation example for create page button
    • fix details
    • add missing strings

--- modules/documentation_provider_type/json_based/JsonBasedDocumentationProviderTypeModule.php
+++ modules/documentation_provider_type/json_based/JsonBasedDocumentationProviderTypeModule.php
@@ -3,10 +3,13 @@
 class JsonBasedDocumentationProviderTypeModule extends DocumentationProviderTypeModule {
 
 	public function metadataForAllParts() {
-		
+		return json_decode(file_get_contents($this->aConfig['metadata_url']), true);
 	}
 
 	public function contentForPart($sDocumentationPart, $sLanguageId) {
-	
+		$sUrl = $this->aConfig['content_url'];
+		$sUrl = str_replace('${language}', $sLanguageId, $sUrl);
+		$sUrl = str_replace('${part}', $sDocumentationPart, $sUrl);
+		return json_decode(file_get_contents($sUrl), true);
 	}
 }
