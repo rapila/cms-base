904347d22aa444d019cc812499ef30fa3229634a
Jürg Messmer <jm@mosaics.ch>
2014-04-04 11:36:56 +0200

    Make sure to set the template on cloned parts

--- lib/classes/TemplatePart.php
+++ lib/classes/TemplatePart.php
@@ -4,8 +4,15 @@ abstract class TemplatePart {
 	protected $oTemplate;
 
 	public function destroy() {
+		if(!($this->oTemplate instanceof Template)) {
+			ErrorHandler::log($this);
+			ErrorHandler::trace();
+		}
 		$this->oTemplate->replaceAt($this, null);
 		$this->oTemplate = null;
+	}
+	public function setTemplate($oTemplate) {
+		$this->oTemplate = $oTemplate;
 	}
 	public abstract function __toString();
 }
