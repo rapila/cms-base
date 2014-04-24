e0aa2f89bd28b520b16efce3d00f48fb9ce95d0d
Raphael Schweikert <any@sabberworm.com>
2014-04-25 01:28:16 +0200

    Test implied orderings

--- lib/tests/UtilSystemPartTests.php
+++ lib/tests/UtilSystemPartTests.php
@@ -44,4 +44,33 @@ class UtilSystemPartTests extends PHPUnit_Framework_TestCase {
 		$this->setExpectedException('Exception');
 		$aParts = SystemPart::orderedParts($aParts);
 	}
+	
+	public function testImpliedOrdering() {
+		$aParts = array();
+		$aParts[] = SystemPart::getPart('base');
+		$aParts = array_map(function($oPart) {
+			return $oPart->getPrefix();
+		}, SystemPart::orderedParts($aParts));
+		$this->assertSame(array('base', 'site'), $aParts);
+	}
+	
+	public function testImpliedOrderingWithPlugin() {
+		$aParts = array();
+		$aParts[] = SystemPart::getPart('plugins/gaga');
+		$aParts[] = SystemPart::getPart('base');
+		$aParts = array_map(function($oPart) {
+			return $oPart->getPrefix();
+		}, SystemPart::orderedParts($aParts));
+		$this->assertSame(array('base', 'plugins/gaga', 'site'), $aParts);
+	}
+	
+	public function testImpliedOrderingWithPluginForced() {
+		$aParts = array();
+		$aParts[] = SystemPart::getPart('base');
+		$aParts[] = SystemPart::getPart('plugins/gaga', array('dependencies' => array('base' => true)));
+		$aParts = array_map(function($oPart) {
+			return $oPart->getPrefix();
+		}, SystemPart::orderedParts($aParts));
+		$this->assertSame(array('base', 'plugins/gaga', 'site'), $aParts);
+	}
 }
