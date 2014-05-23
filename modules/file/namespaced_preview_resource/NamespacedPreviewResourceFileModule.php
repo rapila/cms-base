012a163e1fd77ef3153766dec5cdec4317ec781b
Raphael Schweikert <any@sabberworm.com>
2014-05-23 10:59:32 +0200

    Allow a widget to opt out of CSS namespacing
    To opt out, declare a constant
    
    	const USE_NAMESPACED_CSS = false;
    
    in the widget class.

--- modules/file/namespaced_preview_resource/NamespacedPreviewResourceFileModule.php
+++ modules/file/namespaced_preview_resource/NamespacedPreviewResourceFileModule.php
@@ -7,6 +7,14 @@ class NamespacedPreviewResourceFileModule extends TemplateResourceFileModule {
 	
 	public function renderFile() {
 		$oTemplate = new Template("$this->sModuleName.$this->sModuleType.$this->sResourceType", array(DIRNAME_MODULES, $this->sModuleType, $this->sModuleName, DIRNAME_TEMPLATES));
-		NamespacedPreviewCssFileModule::processCSSContent($oTemplate->render(), ResourceFinder::findResourceObject(array(DIRNAME_MODULES, $this->sModuleType, $this->sModuleName, DIRNAME_TEMPLATES, "$this->sModuleName.$this->sModuleType.$this->sResourceType.tmpl")));
+		$sContents = $oTemplate->render();
+		$sModuleClass = Module::getClassNameByTypeAndName($this->sModuleType, $this->sModuleName);
+		if($this->sResourceType === ResourceIncluder::RESOURCE_TYPE_CSS && $sModuleClass::USE_NAMESPACED_CSS) {
+			$oFile = ResourceFinder::findResourceObject(array(DIRNAME_MODULES, $this->sModuleType, $this->sModuleName, DIRNAME_TEMPLATES, "$this->sModuleName.$this->sModuleType.$this->sResourceType.tmpl"));
+			NamespacedPreviewCssFileModule::processCSSContent($sContents, $oFile);
+		} else {
+			header("Content-Type: text/css;charset=".Settings::getSetting('encoding', 'browser', 'utf-8'));
+			print($sContents);
+		}
 	}
 }
