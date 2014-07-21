fee1a24fa2145d4be8576e47260e2f7b5bf99187
Jürg Messmer <jm@mosaics.ch>
2014-07-21 13:04:12 +0200

    add boolean refresh to image preview
    • lists of images should not refresh, should cash the image
    • only on change of a document detail the output should always be refreshed

--- modules/widget/document_thumbnail_view/DocumentThumbnailViewWidgetModule.php
+++ modules/widget/document_thumbnail_view/DocumentThumbnailViewWidgetModule.php
@@ -3,9 +3,9 @@
  * @package modules.widget
  */
 class DocumentThumbnailViewWidgetModule extends PersistentWidgetModule {
-	
+
 	public $oDocumentsViewWidgetDelegate;
-	
+
 	public function __construct($sSessionKey = null, $oDocumentsViewWidgetDelegate = null) {
 		parent::__construct($sSessionKey);
 		if($oDocumentsViewWidgetDelegate === null) {
@@ -13,7 +13,7 @@ class DocumentThumbnailViewWidgetModule extends PersistentWidgetModule {
 		}
 		$this->oDocumentsViewWidgetDelegate = $oDocumentsViewWidgetDelegate;
 	}
-	
+
 	public function listDocuments($iThumbnailSize) {
 		$aResults = array();
 		foreach($this->oDocumentsViewWidgetDelegate->getDelegateProxy()->getListContents() as $oDocument) {
@@ -21,23 +21,23 @@ class DocumentThumbnailViewWidgetModule extends PersistentWidgetModule {
 		}
 		return $aResults;
 	}
-	
+
 	private function rowData($oDocument, $iThumbnailSize) {
 		return array('name' => $oDocument->getName(), 'description' => $oDocument->getDescription(), 'id' => $oDocument->getId(), 'preview' => $this->thumbnail($oDocument->getId(), $iThumbnailSize));
 	}
-	
+
 	public function deleteDocument($aDocumentData) {
 		$this->oDocumentsViewWidgetDelegate->getDelegateProxy()->deleteRow($aDocumentData);
 	}
-	
+
 	public function singleDocument($iDocumentId, $iThumbnailSize) {
 		return $this->rowData($this->oDocumentsViewWidgetDelegate->getDelegateProxy()->rowFromData(array('id' => $iDocumentId)), $iThumbnailSize);
 	}
-	
+
 	public function thumbnail($iDocumentId, $iThumbnailSize) {
-		return DocumentQuery::create()->findPk($iDocumentId)->getPreview($iThumbnailSize);
+		return DocumentQuery::create()->findPk($iDocumentId)->getPreview($iThumbnailSize, false);
 	}
-		
+
 	public function setAllowsMultiselect($bInitialAllowsMultiselect) {
 		$this->setSetting('allows_multiselect', $bInitialAllowsMultiselect);
 	}
@@ -45,7 +45,7 @@ class DocumentThumbnailViewWidgetModule extends PersistentWidgetModule {
 	public function getElementType() {
 		return 'div';
 	}
-	
+
 	public function setDocumentKind($sDocumentKind) {
 		return $this->oDocumentsViewWidgetDelegate->setDocumentKind($sDocumentKind);
 	}
@@ -53,7 +53,7 @@ class DocumentThumbnailViewWidgetModule extends PersistentWidgetModule {
 	public function getDocumentKind() {
 		return $this->oDocumentsViewWidgetDelegate->getDocumentKind();
 	}
-	
+
 	public function setDocumentCategoryId($iDocumentCategoryId = null) {
 		return $this->oDocumentsViewWidgetDelegate->setDocumentCategoryId($iDocumentCategoryId);
 	}
@@ -61,7 +61,7 @@ class DocumentThumbnailViewWidgetModule extends PersistentWidgetModule {
 	public function getDocumentCategoryId() {
 		return $this->oDocumentsViewWidgetDelegate->getDocumentCategoryId();
 	}
-	
+
 	public function setSearch($sSearch) {
 		return $this->oDocumentsViewWidgetDelegate->setSearch($sSearch);
 	}
