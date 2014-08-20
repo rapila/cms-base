2715f280d6f73a8d02d88c24e11f53d51db3330e
Jürg Messmer <jm@mosaics.ch>
2014-08-20 11:08:58 +0200

    improve admin dialog css styles
    • make default width of status_section wider because generally the update_by_info and created_by_info requires more space
    • refactor all detail widgets, remove rules that are not necessary anymore, fix dialog width etc
    • make numeric list columns display_type_numeric
    • remove bottom margin for tag_in_detail area so the labels in detail and status_section align
    
    Attn. custom made modules have to be checked because of the change of the status_section styles

--- modules/widget/role_list/RoleListWidgetModule.php
+++ modules/widget/role_list/RoleListWidgetModule.php
@@ -7,24 +7,24 @@ class RoleListWidgetModule extends WidgetModule {
 	private $oListWidget;
 	private $iGroupId;
 	public $oDelegateProxy;
-	
+
 	public function __construct() {
 		$this->oListWidget = new ListWidgetModule();
 		$this->oDelegateProxy = new CriteriaListWidgetDelegate($this, "Role", "role_key");
 		$this->oListWidget->setDelegate($this->oDelegateProxy);
 	}
-	
+
 	public function doWidget() {
 		$aTagAttributes = array('class' => 'role_list');
 		$oListTag = new TagWriter('table', $aTagAttributes);
 		$this->oListWidget->setListTag($oListTag);
 		return $this->oListWidget->doWidget();
 	}
-	
+
 	public function getColumnIdentifiers() {
 		return array('id', 'role_key', 'description', 'user_id', 'group_id', 'delete');
 	}
-	
+
 	public function getMetadataForColumn($sColumnIdentifier) {
 		$aResult = array();
 		switch($sColumnIdentifier) {
@@ -44,10 +44,12 @@ class RoleListWidgetModule extends WidgetModule {
 			case 'user_id':
 				$aResult['heading'] = StringPeer::getString('wns.role.user_with_role_count');
 				$aResult['field_name'] = 'user_with_role_count';
+				$aResult['display_type'] = ListWidgetModule::DISPLAY_TYPE_NUMERIC;
 				break;
 			case 'group_id':
 				$aResult['heading'] = StringPeer::getString('wns.role.group_with_role_count');
 				$aResult['field_name'] = 'group_with_role_count';
+				$aResult['display_type'] = ListWidgetModule::DISPLAY_TYPE_NUMERIC;
 				break;
 			case 'delete':
 				$aResult['heading'] = ' ';
@@ -57,13 +59,13 @@ class RoleListWidgetModule extends WidgetModule {
 		}
 		return $aResult;
 	}
-	
+
 	public function getFilterTypeForColumn($sColumn) {
 		if($sColumn === 'group_id') {
 			return CriteriaListWidgetDelegate::FILTER_TYPE_MANUAL;
 		}
 	}
-	
+
 	public function getGroupName() {
 		if($this->oDelegateProxy->getGroupId() !== CriteriaListWidgetDelegate::SELECT_ALL) {
 			$oGroup = GroupQuery::create()->findPk($this->oDelegateProxy->getGroupId());
@@ -73,12 +75,12 @@ class RoleListWidgetModule extends WidgetModule {
 		}
 		return $this->oDelegateProxy->getGroupId();
 	}
-	
+
 	public function deleteRow($aRowData, $oCriteria) {
 		$oRole = RoleQuery::create()->findPk($aRowData['id']);
 		if($oRole) return $oRole->delete();
 	}
-	
+
 	public function getGroupHasRoles($iGroupId) {
 		return GroupRoleQuery::create()->filterByGroupId($iGroupId)->count() > 0;
 	}
