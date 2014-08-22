9f70939dfdeb8393e0f0f2677115f5640015efbe
Jürg Messmer <jm@mosaics.ch>
2014-08-22 12:41:53 +0200

    fix re-sorting of page objects
    • moving inside existing container
    • moving into new container
    • stop propagation on removing objects forcefully
    • add migration to fix incorrect ordering

--- lib/model/ContentObject.php
+++ lib/model/ContentObject.php
@@ -4,9 +4,9 @@ require_once 'model/om/BaseContentObject.php';
 /**
  * classname ContentObject
  * @package model
- */ 
+ */
 class ContentObject extends BaseContentObject {
-	
+
 	public function getLanguageObject($sLanguageId = null) {
 		if($sLanguageId === null) {
 			$sLanguageId = Session::language();
@@ -17,25 +17,58 @@ class ContentObject extends BaseContentObject {
 		}
 		return $oResult[0];
 	}
-		
+
 	public function getLanguageObjectsByLanguage($sLanguage) {
 		$oCriteria = new Criteria();
 		$oCriteria->add(LanguageObjectPeer::LANGUAGE_ID, $sLanguage);
 		return $this->getLanguageObjects($oCriteria);
 	}
-	
+
 	public function hasLanguage($sLanguageId) {
 		return $this->getLanguageObjectsByLanguage($sLanguageId) !== null;
 	}
-		
+
 	public function getObjectTypeName($sLanguageId=null) {
 		return FrontendModule::getDisplayNameByName($this->getObjectType(), $sLanguageId);
 	}
-	
+
 	public function postSave(PropelPDO $oConnection = null) {
 		PagePeer::ignoreRights(true);
 		//Mark page as updated to flush full page caches
 		$this->getPage()->save();
 		PagePeer::ignoreRights(false);
+	}
+
+	public function sortIntoNew($sNewContainer = null, $iNewPosition = null) {
+		$sOldContainer = $this->getContainerName();
+		$iOldPosition = $this->getSort();
+		//Phase 1: Resort existing
+		if($sOldContainer !== null && $iOldPosition !== null) {
+			$this->prepareContainerForReSort($sOldContainer, $iOldPosition, PHP_INT_MAX);
+		}
+		//Phase 2: Prepare list of new container for insertion
+		if($sNewContainer !== null && $iNewPosition !== null) {
+			$this->prepareContainerForReSort($sNewContainer, PHP_INT_MAX, $iNewPosition);
+		}
+		$this->setContainerName($sNewContainer);
+		$this->setSort($iNewPosition);
+	}
+
+	public function sortInsideExisting($iNewPosition) {
+		$sContainer = $this->getContainerName();
+		$iOldPosition = $this->getSort();
+		$this->prepareContainerForReSort($sContainer, $iOldPosition, $iNewPosition);
+		$this->setSort($iNewPosition);
+	}
+
+	private function prepareContainerForReSort($sNewContainer, $iOldPosition, $iNewPosition) {
+		$iDelta = $iNewPosition > $iOldPosition ? -1 : 1;
+		$iMinPos = min($iOldPosition, $iNewPosition);
+		$iMaxPos = max($iOldPosition, $iNewPosition);
+		$oQuery = ContentObjectQuery::create()->orderBySort();
+		foreach($oQuery->filterById($this->getId(), "<>")->filterByPageId($this->getPageId())->filterByContainerName($sNewContainer)->filterBySort($iMinPos, ">=")->filterBySort($iMaxPos, "<=")->find() as $oObject) {
+			$oObject->setSort($oObject->getSort()+$iDelta);
+			$oObject->save();
+		}
 	}
 }
