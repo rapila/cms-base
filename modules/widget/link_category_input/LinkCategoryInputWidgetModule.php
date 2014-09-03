fd565c63e4e3753dd145235d151573968a9d0e2c
Jürg Messmer <jm@mosaics.ch>
2014-09-03 09:35:58 +0200

    fix sort order of options
    • make sure that only numeric values or an empty string are set

--- modules/widget/link_category_input/LinkCategoryInputWidgetModule.php
+++ modules/widget/link_category_input/LinkCategoryInputWidgetModule.php
@@ -3,12 +3,16 @@
  * @package modules.widget
  */
 class LinkCategoryInputWidgetModule extends WidgetModule {
-	
+
 	public function listCategories() {
 		$oQuery = LinkCategoryQuery::create()->orderByName();
 		if(Settings::getSetting('admin', 'hide_externally_managed_link_categories', true)) {
 			$oQuery->filterByIsExternallyManaged(false);
 		}
-		return $oQuery->select(array('Id', 'Name'))->find()->toKeyValue('Id', 'Name');
+		$aResult = array();
+		foreach($oQuery->select(array('Id', 'Name'))->find() as $aData) {
+			$aResult[] = array('key' => $aData['Id'], 'value' => $aData['Name']);
+		}
+		return $aResult;
 	}
 }
