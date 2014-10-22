f07161868bb83c6d5393e08b69830679dc1ac29a
Jürg Messmer <jm@mosaics.ch>
2014-10-22 11:33:09 +0200

    remove double occurrences of separators in path

--- lib/model/Tag.php
+++ lib/model/Tag.php
@@ -4,7 +4,7 @@ require_once 'model/om/BaseTag.php';
 
 /**
  * @package model
- */ 
+ */
 class Tag extends BaseTag {
 	public function getAllCorrespondingDataEntries($mType = null) {
 		$aResults = array();
@@ -25,30 +25,30 @@ class Tag extends BaseTag {
 		}
 		return $aResults;
 	}
-	
+
 	public function setName($sNewName) {
-		$sNewName = StringUtil::normalize($sNewName);
+		$sNewName = StringUtil::normalizePath($sNewName);
 		parent::setName($sNewName);
 	}
 
 	public function getReadableName() {
 		return StringUtil::makeReadableName($this->getName());
 	}
-	
+
 	public function reloadInstances() {
 		$this->collTagInstances=null;
 	}
-	
+
 	public function getTagInstanceCount() {
 		return $this->countTagInstances();
 	}
-	
+
 	public function countTagInstancesByModel($sModelName) {
 	  $oCriteria = new Criteria();
 	  $oCriteria->add(TagInstancePeer::MODEL_NAME, $sModelName);
 	  return $this->countTagInstances($oCriteria);
 	}
-	
+
 	public function getLanguageIdsOfStrings() {
 		$aLanguages = StringQuery::create()->filterByStringKey('tag.'.$this->getName())->select('LanguageId')->find()->toArray();
 		if(is_array($aLanguages) && !empty($aLanguages)) {
