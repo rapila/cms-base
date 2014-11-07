9933137ab2db04d0e36aa3a893732a936f9bd9fa
Jürg Messmer <jm@mosaics.ch>
2014-11-07 06:52:25 +0100

    remove trailing spaces

--- lib/model/TagQuery.php
+++ lib/model/TagQuery.php
@@ -5,14 +5,14 @@
  * @package    propel.generator.model
  */
 class TagQuery extends BaseTagQuery {
-	
+
 	public function filterByTagged($sModel = null, $iId = null) {
 		$q = $this->distinct()->useTagInstanceQuery();
 		$q->filterByTagged($sModel, $iId);
 		$q->endUse();
 		return $this;
 	}
-	
+
 	public function exclude($iTagId) {
 		if(is_array($iTagId)) {
 			return $this->filterById($iTagId, Criteria::NOT_IN);
@@ -21,10 +21,10 @@ class TagQuery extends BaseTagQuery {
 	}
 	/**
 	* withTagInstanceCountFilteredByModel()
-	* 
+	*
 	* @param string model name, optional
 	* @param array tagged_item_id, optional
-	* 
+	*
 	* Description
 	* we need both model and included tagged_item_ids in order to create a list of tags
 	* that are usefull in the context, i.e. journal. All Tags should be related to entries of the configured journal
