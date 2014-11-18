60958892caeba3f352366256cf161b2672ed90e2
Jürg Messmer <jm@mosaics.ch>
2014-11-18 11:31:16 +0100

    add method to make common task easy

--- lib/model/PageQuery.php
+++ lib/model/PageQuery.php
@@ -5,7 +5,7 @@
  * @package    propel.generator.model
  */
 class PageQuery extends BasePageQuery {
-	
+
 	public function filterByParentAndName($sName, $mParent, $mCurrentToExclude = null) {
 		if(!($mParent instanceof Page)) {
 			$mParent = PageQuery::create()->findPk($mParent);
@@ -19,7 +19,7 @@ class PageQuery extends BasePageQuery {
 		}
 		return $this;
 	}
-	
+
 	public function descendantsOf($oPage) {
 		// Default implementation of childrenOf assumes $oPage is non-null.
 		if($oPage !== null) {
@@ -35,9 +35,13 @@ class PageQuery extends BasePageQuery {
 		}
 		return parent::childrenOf($oPage);
 	}
-	
+
 	public function active($bIsActive = true) {
 		return $this->filterByIsInactive(!$bIsActive);
+	}
+
+	public function filterByFirstLevelNameOrIdentifier($sName) {
+		return $this->filterByName($sName)->_and()->filterByTreeLevel(1)->_or()->filterByIdentifier($sName);
 	}
 }
 
