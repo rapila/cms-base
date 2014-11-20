2c8296e1c03532620b9bdeaae57e43b52bc86227
Jürg Messmer <jm@mosaics.ch>
2014-11-20 14:22:23 +0100

    remove trailing spaces

--- lib/classes/NavigationItem.php
+++ lib/classes/NavigationItem.php
@@ -6,30 +6,30 @@ abstract class NavigationItem {
 	protected $oParent;
 	protected $aCustomChildren;
 	protected $aChildren;
-	
+
 	public function __construct($oParent) {
 		$this->oParent = $oParent;
 		$this->aChildren = array();
 		$this->aCustomChildren = null;
 	}
-	
+
 	public function getLevel() {
 		if($this->isRoot()) {
 			return 0;
 		}
 		return $this->oParent->getLevel()+1;
 	}
-	
+
 	protected abstract function getChildrenImpl($sLanguageId, $bIncludeDisabled, $bIncludeInvisible);
 	protected abstract function hasChildrenImpl($sLanguageId, $bIncludeDisabled, $bIncludeInvisible);
-	
+
 	private function prepareChildren() {
 		if($this->aCustomChildren === null) {
 			$this->aCustomChildren = array();
 			FilterModule::getFilters()->handleNavigationItemChildrenRequested($this);
 		}
 	}
-	
+
 	protected function getCustomChildren($sLanguageId = null, $bIncludeDisabled = false, $bIncludeInvisible = false) {
 		//Language id is ignored in this implementation
 		if($bIncludeDisabled && $bIncludeInvisible) {
@@ -45,7 +45,7 @@ abstract class NavigationItem {
 		}
 		return $aCustomChildren;
 	}
-	
+
 	protected function hasCustomChildren($sLanguageId = null, $bIncludeDisabled = false, $bIncludeInvisible = false) {
 		//Language id is ignored in this implementation
 		if($bIncludeDisabled && $bIncludeInvisible) {
@@ -60,7 +60,7 @@ abstract class NavigationItem {
 		}
 		return false;
 	}
-	
+
 	public function getChildren($sLanguageId = null, $bIncludeDisabled = false, $bIncludeInvisible = false) {
 		$sKey = "{$sLanguageId}_{$bIncludeDisabled}_{$bIncludeInvisible}";
 		if(!isset($this->aChildren[$sKey])) {
@@ -69,7 +69,7 @@ abstract class NavigationItem {
 		$this->prepareChildren();
 		return ($this->aChildren[$sKey] + $this->getCustomChildren($sLanguageId, $bIncludeDisabled, $bIncludeInvisible));
 	}
-	
+
 	public function hasChildren($sLanguageId = null, $bIncludeDisabled = false, $bIncludeInvisible = false) {
 		if($this->hasChildrenImpl($sLanguageId, $bIncludeDisabled, $bIncludeInvisible)) {
 			return true;
@@ -77,29 +77,29 @@ abstract class NavigationItem {
 		$this->prepareChildren();
 		return $this->hasCustomChildren($sLanguageId, $bIncludeDisabled, $bIncludeInvisible);
 	}
-	
+
 	public function isRoot() {
 		return $this->oParent === null;
 	}
-	
+
 	public abstract function isCurrent();
 	public abstract function isActive();
 	public abstract function isSiblingOfCurrent();
-	
+
 	public function isSiblingOfActive() {
 		if($this->isRoot()) {
 			return false;
 		}
 		return !$this->isActive() && !$this->oParent->isCurrent() && $this->oParent->isActive();
 	}
-	
+
 	public function isChildOfCurrent() {
 		if($this->isRoot()) {
 			return false;
 		}
 		return $this->oParent->isCurrent();
 	}
-	
+
 	public function isDescendantOfCurrent() {
 		if($this->isRoot()) {
 			return false;
@@ -109,24 +109,24 @@ abstract class NavigationItem {
 		}
 		return $this->oParent->isDescendantOfCurrent();
 	}
-	
+
 	public abstract function getTitle($sLanguageId = null);
 	public abstract function getLinkText($sLanguageId = null);
 	public abstract function getDescription($sLanguageId = null);
 	public abstract function getName();
 	public abstract function getType();
-	
+
 	public function getIdentifier() {
 		return null;
 	}
-	
+
 	public function getLink() {
 		if($this->isRoot()) {
 			return array();
 		}
 		return array_merge($this->oParent->getLink(), array($this->getName()));
 	}
-	
+
 	/**
 	* Gets a value uniquely identifying this navigation item. This could be a full path or an internal id of the backing store.
 	* Note: This is NOT the same as the identifier retrieved using @link(getIdentifier()) as that is allowed to return null in some implementations.
@@ -137,11 +137,11 @@ abstract class NavigationItem {
 		}
 		return "{$this->oParent->getId()}/{$this->getName()}";
 	}
-	
+
 	public function getParent() {
 		return $this->oParent;
 	}
-	
+
 	public function getFirstChild($sLanguageId = null, $bIncludeDisabled = false, $bIncludeInvisible = false) {
 		foreach($this->getChildren($sLanguageId, $bIncludeDisabled, $bIncludeInvisible) as $oChild) {
 			if($oChild->isFolder()) {
@@ -151,9 +151,9 @@ abstract class NavigationItem {
 		}
 		return null;
 	}
-	
+
 	protected abstract function getCanonicalImpl($sLanguageId = null);
-	
+
 	public function getCanonical($sLanguageId = null) {
 		$oCanonical = $this->getCanonicalImpl($sLanguageId);
 		if($oCanonical) {
@@ -168,7 +168,7 @@ abstract class NavigationItem {
 		}
 		return $oParentCanonical->namedChild($this->getName(), $sLanguageId, !$this->isEnabled(), !$this->isVisible());
 	}
-	
+
 	public abstract function isProtected();
 	public function isAccessible() {
 		if(!$this->isProtected()) {
@@ -180,13 +180,13 @@ abstract class NavigationItem {
 	public abstract function isEnabled();
 	public abstract function isFolder();
 	public abstract function isVirtual();
-	
+
 	public function addChild(NavigationItem $oChild) {
 		$this->prepareChildren();
 		$oChild->oParent = $this;
 		$this->aCustomChildren[$oChild->getName()] = $oChild;
 	}
-	
+
 	public function namedChild($sName, $sLanguageId = null, $bIncludeDisabled = false, $bIncludeInvisible = false) {
 		$aChildren = $this->getChildren($sLanguageId, $bIncludeDisabled, $bIncludeInvisible);
 		if(isset($aChildren[$sName])) {
