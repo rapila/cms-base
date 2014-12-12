fd771b095f62c4dbbe51947e3c2e6478a89b9f3f
Raphael Schweikert <any@sabberworm.com>
2014-12-12 14:45:47 +0100

    Expose resource includer to subclasses

--- lib/classes/AdminManager.php
+++ lib/classes/AdminManager.php
@@ -10,7 +10,7 @@ class AdminManager extends Manager {
 
 	private $sModuleName;
 	private $oModule;
-	private $oResourceIncluder;
+	protected $oResourceIncluder;
 
 	public function __construct() {
 		parent::__construct();
