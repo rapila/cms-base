de42c596bfe5105e0a17934c52bcc32a2e7e1341
Jürg Messmer <jm@mosaics.ch>
2015-03-13 15:02:24 +0100

    Tag on actual tag model of object
    (as per taggable behaviour configuration)
    
    …instead of hardcoding “Tag”

--- lib/model/TagPeer.php
+++ lib/model/TagPeer.php
@@ -2,25 +2,25 @@
 
 	// include base peer class
 	require_once 'model/om/BaseTagPeer.php';
-	
+
 	// include object class
 	include_once 'model/Tag.php';
 
 
 /**
  * @package model
- */ 
+ */
 class TagPeer extends BaseTagPeer {
-	
+
 	public static function addSearchToCriteria($sSearch, $oCriteria) {
 		$oCriteria->add($oCriteria->getNewCriterion(self::NAME, "%$sSearch%", Criteria::LIKE));
 	}
-	
+
 	/**
 	* @deprecated use query methods
 	*/
 	public static function allTaggedIdsForModel($sModelName, $sTagName = null) {
-		$oCriteria = TagInstanceQuery::create()->innerJoinTag()->filterByModelName($sModelName);
+		$oCriteria = $sInstanceQuery::create()->innerJoinTag()->filterByModelName($sModelName);
 		if($sTagName !== null) {
 			$oCriteria->add(TagPeer::NAME, $sTagName);
 		}
@@ -32,21 +32,20 @@ class TagPeer extends BaseTagPeer {
 		return '*';
 	}
 
-	public static function droppedOnto($mDroppedId, $sDroppableModelName, $mDroppableId) {
-		$oQuery = TagInstanceQuery::create()->filterByTagName($mDroppedId);
+	public static function droppedOnto($mDroppedId, $sDroppableModelName, $sTagName) {
+		$sInstanceModel = $sDroppableModelName::TAG_INSTANCE_MODEL_NAME;
+		$sInstanceQuery = "${sInstanceModel}Query";
+
+		$oQuery = $sInstanceQuery::create()->useTagQuery()->filterByName($mDroppedId)->endUse();
 		$oResult = new stdClass();
 		$oResult->status = 'tagged';
 		$oResult->is_new = $oQuery->count() === 0;
 		$oResult->is_new_to_model = $oResult->is_new || $oQuery->filterByModelName($sDroppableModelName)->count() === 0;
-		$oResult->is_first_of_model = TagInstanceQuery::create()->filterByModelName($sDroppableModelName)->count() === 0;
-		try {
-			TagInstancePeer::newTagInstance($mDroppedId, $sDroppableModelName, $mDroppableId);
-		} catch (Exception $e) {
-			$oResult->status = 're-tagged';
-		}
+		$oResult->is_first_of_model = $sInstanceQuery::create()->filterByModelName($sDroppableModelName)->count() === 0;
+		$oInstance = $sDroppableModelName::addTagTo($sTagName, $mDroppedId);
 		return $oResult;
 	}
-	
+
 	/**
 	* @deprecated use query methods
 	*/
@@ -61,31 +60,31 @@ class TagPeer extends BaseTagPeer {
 		$oCriteria->addAscendingOrderByColumn(self::NAME);
 		return self::doSelect($oCriteria);
 	}
-	
+
 	public static function retrieveByName($sTagName) {
 		$oCriteria = new Criteria(self::DATABASE_NAME);
 		$oCriteria->add(self::NAME, StringUtil::normalize($sTagName));
 		return self::doSelectOne($oCriteria);
 	}
-	
+
 	public static function deleteTagsForObject($oObject) {
 		$aTagInstances = self::tagInstancesForObject($oObject);
 		foreach($aTagInstances as $oTagInstance) {
 			$oTagInstance->delete();
 		}
 	}
-	
+
 	public static function tagInstancesForObject($oObject) {
 		return self::tagInstancesForModel(get_class($oObject), $oObject->getPKString());
 	}
-	
+
 	public static function tagInstancesForModel($sModelName, $iTaggedItemId) {
 		$oCriteria = new Criteria();
 		$oCriteria->add(TagInstancePeer::TAGGED_ITEM_ID, $iTaggedItemId);
 		$oCriteria->add(TagInstancePeer::MODEL_NAME, $sModelName);
 		return TagInstancePeer::doSelect($oCriteria);
 	}
-	
+
 	public static function getTagsUsedInModelCriteria($sModelName) {
 		$oCriteria = new Criteria();
 		$oCriteria->setDistinct();
@@ -94,7 +93,7 @@ class TagPeer extends BaseTagPeer {
 		$oCriteria->addAscendingOrderByColumn(self::NAME);
 		return $oCriteria;
 	}
-	
+
 	public static function getTagsWithTaggedItemCountByModelName($sModelName) {
 	  $oCriteria = new Criteria();
 		$oCriteria->addJoin(self::ID, TagInstancePeer::TAG_ID, Criteria::INNER_JOIN);
--- modules/widget/tag_area/TagAreaWidgetModule.php
+++ modules/widget/tag_area/TagAreaWidgetModule.php
@@ -5,23 +5,26 @@
 */
 
 class TagAreaWidgetModule extends PersistentWidgetModule {
-	
+
 	const USE_NAMESPACED_CSS = false;
-	
+
 	private $sModelName;
 	private $mTaggedItemId;
-	
+
 	public function listTags() {
 		$oQuery = TagQuery::create();
 		$oQuery->filterByTagged($this->sModelName, $this->mTaggedItemId);
 		return $oQuery->find()->toArray();
 	}
-	
+
 	public function deleteTag($sTagName) {
 		if($this->sModelName === null || $this->mTaggedItemId === null) {
 			throw new Exception('Can only delete specific tags');
 		}
-		$oTag = TagInstanceQuery::create()->filterByModelName($this->sModelName)->filterByTaggedItemId($this->mTaggedItemId)->filterByTagName($sTagName)->findOne();
+		$sModel = $this->sModelName;
+		$sInstanceModel = $sModel::TAG_INSTANCE_MODEL_NAME;
+		$sInstanceQuery = "${sInstanceModel}Query";
+		$oTag = $sInstanceQuery::create()->filterByModelName($this->sModelName)->filterByTaggedItemId($this->mTaggedItemId)->useTagQuery()->filterByName($sTagName)->endUse()->findOne();
 		$oResult = new stdClass();
 		$oResult->model_name = $this->sModelName;
 		$oResult->tagged_item_id = $this->mTaggedItemId;
@@ -30,14 +33,14 @@ class TagAreaWidgetModule extends PersistentWidgetModule {
 			$oResult->is_removed_from_model = false;
 		} else {
 			$oTag->delete();
-			$oQuery = TagInstanceQuery::create()->filterByTagName($sTagName);
+			$oQuery = $sInstanceQuery::create()->useTagQuery()->filterByName($sTagName)->endUse();
 			$oResult->is_removed = $oQuery->count() === 0;
 			$oResult->is_removed_from_model = $oResult->is_removed || $oQuery->filterByModelName($this->sModelName)->count() === 0;
-			$oResult->was_last_of_model = $oResult->is_removed_from_model && TagInstanceQuery::create()->filterByModelName($this->sModelName)->count() === 0;
+			$oResult->was_last_of_model = $oResult->is_removed_from_model && $sInstanceQuery::create()->filterByModelName($this->sModelName)->count() === 0;
 		}
 		return $oResult;
 	}
-	
+
 	public function tagId($sTagName) {
 		$oTag = TagQuery::create()->filterByName($sTagName)->findOne();
 		if($oTag === null) {
@@ -45,7 +48,7 @@ class TagAreaWidgetModule extends PersistentWidgetModule {
 		}
 		return $oTag->getId();
 	}
-	
+
 	public function setModelName($sModelName) {
 		$this->sModelName = $sModelName;
 	}
@@ -53,7 +56,7 @@ class TagAreaWidgetModule extends PersistentWidgetModule {
 	public function getModelName() {
 		return $this->sModelName;
 	}
-	
+
 	public function setTaggedItemId($mTaggedItemId) {
 		$this->mTaggedItemId = $mTaggedItemId;
 	}
