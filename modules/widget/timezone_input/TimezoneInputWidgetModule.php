846a78791234ecf2fdf468ce4d25dd01bc7475fe
Raphael Schweikert <any@sabberworm.com>
2015-04-21 17:06:40 +0200

    Fix default timezone in select for new user

--- modules/widget/timezone_input/TimezoneInputWidgetModule.php
+++ modules/widget/timezone_input/TimezoneInputWidgetModule.php
@@ -1,6 +1,8 @@
 <?php
 class TimezoneInputWidgetModule extends WidgetModule {
+
 	public function getElementType() {
+		$this->setSetting('default_value', Settings::getSetting('general', 'timezone', 'Europe/Zurich'));
 		return 'select';
 	}
 	
