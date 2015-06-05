d0d4baa399237f9aa36c0ce134cd0d6c550ffa6f
Thomas Gressly <founder@foundit.ch>
2015-06-05 14:37:41 +0200

    Add mail address to exception

--- lib/classes/EMail.php
+++ lib/classes/EMail.php
@@ -3,44 +3,44 @@
 * @package email
 */
 class EMail {
-	
+
 	private $aRecipients;
-	
+
 	private $aCarbonCopyRecipients;
-	
+
 	private $aBlindCarbonCopyRecipients;
-	
+
 	private $sCharset;
-	
+
 	private $sMimeType;
-	
+
 	private $oContent;
-	
+
 	private $sSenderName;
-	
+
 	private $sSenderAddress;
-	
+
 	private $sReplyTo;
-	
+
 	private $sSubject;
-	
+
 	private $oFlash;
-	
+
 	const SEPARATOR = "\n";
-	
+
  /** __construct()
 	* @param string subject
 	* @param mixed Template|String
 	* @param boolean is html email
 	* @param boolean use multi part
-	* 
+	*
 	* @return void
 	*/
 	public function __construct($sSubject, $oContent = null, $bIsHtml = false) {
 		$this->aRecipients = array();
 		$this->aCarbonCopyRecipients = array();
 		$this->aBlindCarbonCopyRecipients = array();
-		
+
 		if($oContent instanceof Template) {
 			$this->sCharset = $oContent->getCharset();
 		} else {
@@ -54,12 +54,12 @@ class EMail {
 		$this->sSenderName = null;
 		$this->sSenderAddress = LinkUtil::getDomainHolderEmail('mailer-daemon');
 		$this->oFlash = new Flash();
-		
+
 		$this->sReplyTo = null;
-		
+
 		$this->sSubject = $sSubject;
 	}
-	
+
 	public function setCharset($sCharset) {
 		$this->sCharset = $sCharset;
 	}
@@ -67,7 +67,7 @@ class EMail {
 	public function getCharset() {
 		return $this->sCharset;
 	}
-	
+
 	public function setTemplate($oContent) {
 		$this->oContent = $oContent;
 	}
@@ -75,7 +75,7 @@ class EMail {
 	public function getTemplate() {
 		return $this->oContent;
 	}
-	
+
 	public function setRecipients($aRecipients) {
 		$this->aRecipients = $aRecipients;
 	}
@@ -83,7 +83,7 @@ class EMail {
 	public function getRecipients() {
 		return $this->aRecipients;
 	}
-	
+
 	public function setCarbonCopyRecipients($aCarbonCopyRecipients) {
 		$this->aCarbonCopyRecipients = $aCarbonCopyRecipients;
 	}
@@ -91,7 +91,7 @@ class EMail {
 	public function getCarbonCopyRecipients() {
 		return $this->aCarbonCopyRecipients;
 	}
-	
+
 	public function setBlindCarbonCopyRecipients($aBlindCarbonCopyRecipients) {
 		$this->aBlindCarbonCopyRecipients = $aBlindCarbonCopyRecipients;
 	}
@@ -99,19 +99,19 @@ class EMail {
 	public function getBlindCarbonCopyRecipients() {
 		return $this->aBlindCarbonCopyRecipients;
 	}
-	
+
 	public function addRecipient($sAddress, $sName = null) {
 		$this->aRecipients[$sAddress] = $sName;
 	}
-	
+
 	public function addCarbonCopyRecipient($sAddress, $sName = null) {
 		$this->aCarbonCopyRecipients[$sAddress] = $sName;
 	}
-	
+
 	public function addBlindCarbonCopyRecipient($sAddress, $sName = null) {
 		$this->aBlindCarbonCopyRecipients[$sAddress] = $sName;
 	}
-	
+
 	public function setSender($sSenderName, $sSenderAddress) {
 		$this->sSenderName = $sSenderName;
 		$this->sSenderAddress = $sSenderAddress;
@@ -120,7 +120,7 @@ class EMail {
 	public function getSender() {
 		return array($this->sSenderName, $this->sSenderAddress);
 	}
-	
+
 	public function setReplyTo($sReplyTo) {
 		$this->sReplyTo = $sReplyTo;
 	}
@@ -128,43 +128,43 @@ class EMail {
 	public function getReplyTo() {
 		return $this->sReplyTo;
 	}
-	
+
 	public function send() {
 		$aRecipients = array();
 
 		foreach($this->aRecipients as $sAddress => $sName) {
 			$aRecipients[] = $this->getAddressToken($sName, $sAddress);
 		}
-		
+
 		$sRecipients = implode(', ', $aRecipients);
-		
+
 		if($this->oContent instanceof Template) {
 			$this->oContent = MIMELeaf::leafWithTemplate($this->oContent, $this->sMimeType, '8bit', null, null, $this->sCharset);
 		}
-		
+
 		foreach($this->aCarbonCopyRecipients as $sAddress => $sName) {
 			$this->oContent->addToHeader("Cc", $this->getAddressToken($sName, $sAddress));
 		}
-		
+
 		foreach($this->aBlindCarbonCopyRecipients as $sAddress => $sName) {
 			$this->oContent->addToHeader("Bcc", $this->getAddressToken($sName, $sAddress));
 		}
-		
+
 		if($this->sReplyTo !== null) {
 			$this->oContent->setHeader("Reply-To", $this->sReplyTo);
 		}
-		
+
 		$this->oContent->setHeader('From', $this->getAddressToken($this->sSenderName, $this->sSenderAddress));
-		
+
 		$sSubject = '=?'.Settings::getSetting("encoding", "db", "utf-8").'?B?'.base64_encode($this->sSubject).'?=';
-		
+
 		$bResult = $this->sendMail($sRecipients, $sSubject, $this->oContent->getBody(), $this->oContent->getHeaderString());
 
 		if($bResult === false) {
-			throw new Exception("Error in EMail->send(): E-Mail was not accepted for delivery");
+			throw new Exception("Error in EMail->send(): E-Mail was not accepted for delivery: ".$sRecipients);
 		}
 	}
-	
+
 	private function sendMail($sTo, $sSubject, $sMessage, $sHeaders) {
 		$sSendmailPath = Settings::getSetting('email', 'sendmail_path', null);
 		if($sSendmailPath === null || $sSendmailPath === ini_get('sendmail_path')) {
@@ -205,7 +205,7 @@ class EMail {
 
 class EMailAddressInvalidException extends Exception {
 	private $sAddress;
-	
+
 	public function __construct($sMessage, $sAddress) {
 		parent::__construct($sMessage);
 		$this->sAddress = $sAddress;
