28b46d0f25b539144029dc00da488d59085f8f1b
Raphael Schweikert <any@sabberworm.com>
2015-11-09 14:41:59 +0100

    Skip test on old iconv

--- lib/tests/UtilStringTests.php
+++ lib/tests/UtilStringTests.php
@@ -16,6 +16,13 @@ class UtilStringTests extends PHPUnit_Framework_TestCase {
 		$this->assertSame("remy", StringUtil::normalizeToASCII("rémy"));
 		$this->assertSame("rund-lauf-themen", StringUtil::normalizeToASCII("rund lauf – themen"));
 		$this->assertSame("rund_lauf_-_themen", StringUtil::normalizeToASCII("rund lauf – themen", '_'));
-		$this->assertSame("ruembael-aosduf-o-asso-faoss-duf", StringUtil::normalizeToASCII("rümbäl-aosduf-ø-ˆå¨ß∂ˆø¨-ƒåøßˆ-duf"));
+	}
+	
+	public function testNormalizationOfDiacriticsToASCII() {
+		if(defined('ICONV_VERSION') && version_compare(ICONV_VERSION, '1.11', '>=')) {
+			$this->assertSame("ruembael-aosduf-o-asso-faoss-duf", StringUtil::normalizeToASCII("rümbäl-aosduf-ø-ˆå¨ß∂ˆø¨-ƒåøßˆ-duf"));
+		} else {
+			$this->markTestSkipped('Iconv not present or too old.');
+		}
 	}
 }
