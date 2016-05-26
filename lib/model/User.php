1e432ad2c0df92793ac3880e7605fad7a237324d
Raphael Schweikert <any@sabberworm.com>
2016-05-26 14:41:51 +0200

    Fix typo

--- lib/model/User.php
+++ lib/model/User.php
@@ -23,7 +23,7 @@ class User extends BaseUser {
 	}
 
 	public function getFullNameInverted($sSeparator=', ') {
-		return implode($sSeparator=', ', array_reverse($this->getFullNameArray()));
+		return implode(', ', array_reverse($this->getFullNameArray()));
 	}
 
 	public function getFullNameArray() {
