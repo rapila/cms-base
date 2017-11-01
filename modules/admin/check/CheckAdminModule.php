f5e6ce595944c85344d9170c9b753a5d053fa46d
Jürg Messmer <jm@mosaics.ch>
2017-11-01 17:23:10 +0100

    fix missing implementation of ListWidgetDelegate
    Note: the display of the various check contents is not implemented properly yet, just the sidebar list is fixed

--- modules/admin/check/CheckAdminModule.php
+++ modules/admin/check/CheckAdminModule.php
@@ -2,49 +2,51 @@
 /**
  * @package modules.admin
  */
-class CheckAdminModule extends AdminModule {
-	
+class CheckAdminModule extends AdminModule implements ListWidgetDelegate {
+
 	private $sAction = null;
 	private $oTemplate;
 	private $oCheckOptionWidget;
-	
+	private $aCheckOptions = array("pages", "strings", "static_strings", "config", "tags", "content");
+
 	private static $EMPTY_STRING_KEY = "139c4e89cdbedaf144d05ca54a12a57b";
-	
+
 	private static $CONFIG_SETTINGS_SHOULD = array( "magic_quotes_gpc" => "0",
 																									"safe_mode" => "",
 																									"file_uploads" => "1",
 																									"allow_url_fopen" => "1",
 																									"output_buffering" => "1",
 																									"magic_quotes_runtime" => "");
-	
+
 	private static $CONFIG_SETTINGS_SHOULD_NOT = array( "last_modified" => '1',
 																											"register_globals" => '1');
 	const LOG_LEVEL_INFO = 0;
 	const LOG_LEVEL_NOTICE = 2;
 	const LOG_LEVEL_WARNING = 4;
 	const LOG_LEVEL_ERROR = 6;
-	
+
 	public function __construct() {
 		$this->oCheckOptionWidget = new ListWidgetModule();
+		$this->oCheckOptionWidget->setListTag(new TagWriter('ul'));
 		$this->oCheckOptionWidget->setDelegate($this);
 	}
-	
+
 	public function sidebarContent() {
 		return $this->oCheckOptionWidget->doWidget();
 	}
-	
+
 	public function mainContent() {
-		// return $this->constructTemplate('info');
+		// return $this->constructTemplate('main');
 	}
 
 	public function setAction($sAction) {
 	  $this->sAction = $sAction;
 	}
-	
+
 	public function getColumnIdentifiers() {
 		return array('action', 'title');
 	}
-	
+
 	public function getMetadataForColumn($sColumnIdentifier) {
 		$aResult = array();
 		switch($sColumnIdentifier) {
@@ -57,11 +59,14 @@ class CheckAdminModule extends AdminModule {
 		}
 		return $aResult;
 	}
-	
+
+	public function numberOfRows() {
+		return count($this->aCheckOptions);
+	}
+
 	public function getListContents($iRowStart = 0, $iRowCount = null) {
 		$aResult = array();
-		$aCheckOptions = array("pages", "strings", "static_strings", "config", "tags", "content");
-		foreach($aCheckOptions as $sAction) {
+		foreach($this->aCheckOptions as $sAction) {
 			$aResult[] = array('action' => $sAction, 'title' => TranslationPeer::getString('wns.check.'.$sAction));
 		}
 		if($iRowCount === null) {
@@ -69,7 +74,7 @@ class CheckAdminModule extends AdminModule {
 		}
 		return array_splice($aResult, $iRowStart, $iRowCount);
 	}
-	
+
 	public function usedWidgets() {
 		return array($this->oCheckOptionWidget);
 	}
