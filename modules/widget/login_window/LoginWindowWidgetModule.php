64dc750e839cbbdf219be7912fa932590432e43a
Raphael Schweikert <any@sabberworm.com>
2018-04-30 20:16:59 +0200

    Clear session only when logging out
    Aids back button support (but may blow up session storage if people don’t log out)

--- modules/widget/login_window/LoginWindowWidgetModule.php
+++ modules/widget/login_window/LoginWindowWidgetModule.php
@@ -43,6 +43,8 @@ class LoginWindowWidgetModule extends PersistentWidgetModule {
 	}
 
 	public function logout() {
+		// Clear all widgets
+		Session::getSession()->setAttribute(WidgetModule::WIDGET_SESSION_KEY, null);
 		Session::getSession()->logout();
 		Cache::clearAllCaches();
 		return array('success' => true);
